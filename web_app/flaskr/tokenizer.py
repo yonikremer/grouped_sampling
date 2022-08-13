4806fb46a41f1b12a5e3b4af6c4967ee85ad6d2a
yoni kremer <yoni.kremer@gmail.com>
2022-08-13 17:06:15 +0300

    Fixed import issue

--- web_app/flaskr/tokenizer.py
+++ web_app/flaskr/tokenizer.py
@@ -1,18 +1,17 @@
 """Contains the functions for the tokenizer."""
 
 import tensorflow as tf
-import tensorflow_text as tf_text
+from tensoeflow_text import BertTokenizer
 from flask import g  # A dictionary containing all the global variables.
-from typing import List
 
 
-def init_tokenizer() -> tf_text.BertTokenizer:
+def init_tokenizer():
     """Creates and returns the tokenizer and vocabulary."""
-    path: str = 'flaskr/static/look_up_table.txt'
-    bert_tokenizer_params: dict = dict(lower_case = True)
-    with open(path, 'r') as f:
-        vocab: List[str] = f.read().split()
-    tensor_vocab: List[tf.Tensor] = [tf.convert_to_tensor(token_key, dtype = tf.string) for token_key in vocab]
+    path = 'flaskr/static/look_up_table.txt'
+    bert_tokenizer_params = dict(lower_case = True)
+    with open(path, 'r', encoding="utf-8") as file:
+        vocab = file.read().split()
+    tensor_vocab = [tf.convert_to_tensor(token_key, dtype = tf.string) for token_key in vocab]
     lookup_table = tf.lookup.StaticVocabularyTable(
         tf.lookup.KeyValueTensorInitializer(
             keys = tensor_vocab,
@@ -22,18 +21,18 @@ def init_tokenizer() -> tf_text.BertTokenizer:
             ),
         num_oov_buckets = 1
     )
-    my_tokenizer: tf_text.BertTokenizer = tf_text.BertTokenizer(lookup_table, **bert_tokenizer_params)
-    # tf_text.Tokenizer is the base class for tf_text.BertTokenizer
+    my_tokenizer = BertTokenizer(lookup_table, **bert_tokenizer_params)
     return my_tokenizer, vocab
 
 
-def tokenize_and_preprocces(text: str) -> tf.Tensor:
+def tokenize_and_preprocess(text):
     """Converts string to tensor of tokens of shape [1, seq_length]."""
-    ragged: tf.RaggedTensor = g.my_tokenizer.tokenize(text)[0, :]
-    eager: tf.Tensor = ragged.to_tensor(default_value = 0, shape = [None, 1])  # 0 is the value of the padding token
-    sqeezed: tf.Tensor = tf.squeeze(eager, axis = 1)
-    typed: tf.Tensor = tf.cast(sqeezed, tf.int32)
-    start_token: tf.Tensor = tf.constant([2], dtype=tf.int32)
-    edited: tf.Tensor = tf.concat([start_token, typed], axis = 0)
-    expended: tf.Tensor = tf.expand_dims(edited, axis=0)
+    ragged = g.my_tokenizer.tokenize(text)[0, :]
+    eager = ragged.to_tensor(default_value = 0, shape = [None, 1])
+    # 0 is the value of the padding token
+    squeezed = tf.squeeze(eager, axis = 1)
+    typed = tf.cast(squeezed, tf.int32)
+    start_token = tf.constant([2], dtype=tf.int32)
+    edited = tf.concat([start_token, typed], 0)
+    expended = tf.expand_dims(edited, axis=0)
     return expended
