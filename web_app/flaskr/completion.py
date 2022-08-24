6ffbb52d26ab34dfc1a119f239ee039db900d5ea
yoni kremer <yoni.kremer@gmail.com>
2022-08-24 16:42:17 +0300

    Partialy fixed bar request error

--- web_app/flaskr/completion.py
+++ web_app/flaskr/completion.py
@@ -10,7 +10,7 @@ from torch.nn import Softmax
 from flaskr.auth import login_required
 from flaskr.db import get_db
 from flaskr.tokenizer import tf_preprocess_and_tokenize, init_tf_tokenizer, create_tar
-from flaskr.sampling import grouped_sampling
+from flaskr.sampling import grouped_sampling, seq_prob
 
 
 bp = Blueprint('completion', __name__)
@@ -87,24 +87,24 @@ def complete(model_name, org_prompt, top_p, top_k, num_groups, group_size, org_p
         tokenized_prompt_list = tokenized_prompt_ten.tolist()
     elif isinstance(org_prompt, list):
         tokenized_prompt_list = flatten(org_prompt)
-        str_prompt = tokenizer.decode(tokenized_prompt_list)
     else:
         raise ValueError("org_prompt must be a string or list of integers")
     
     prob_mat = get_prob_mat(model_name, str_prompt, group_size)
-    tokenized_ans_list, prob_list = grouped_sampling(prob_mat, top_p, top_k, group_size)
+    tokenized_ans_list = grouped_sampling(prob_mat, top_p, top_k)
+    prob_list: List[Tuple[List[int], float]] = [seq_prob(seq, prob_mat, org_prompt_prob) for seq in tokenized_ans_list]
     new_prompts: List[List[int]] = [tokenized_prompt_list + ans for ans in tokenized_ans_list]
+    complition_prob_dict = remove_duplicates(new_prompts, prob_list)
     if num_groups == 1:
-        return remove_duplicates(new_prompts, prob_list)
-    all_new_completions: List[List[int]] = []
-    all_new_probs: List[float] = []
-    for curr_new_prompt, curr_new_prompt_prob in zip(new_prompts, prob_list):
-        curr_new_prompt_prob *= org_prompt_prob
-        curr_completions = complete(model_name, curr_new_prompt, top_p, top_k, num_groups - 1, group_size, curr_new_prompt_prob)
+        return complition_prob_dict
+    new_complitions: Dict[Tuple[int], float] = dict()
+    for curr_new_prompt, curr_new_prompt_prob in complition_prob_dict.items():
+        curr_completions: Dict[Tuple[int], float] = complete(model_name, curr_new_prompt, top_p, top_k, num_groups - 1, group_size, curr_new_prompt_prob)
+        tokens: Tuple[int]
+        prob: float
         for tokens, prob in curr_completions.items():
-            all_new_completions.append(tokens)
-            all_new_probs.append(prob)
-    return remove_duplicates(all_new_completions, all_new_probs)
+            new_complitions[tokens] = prob
+    return new_complitions
 
 
 def remove_duplicates(completions: List[List[int]], probs: List[float]) -> Dict[Tuple[int], float]:
@@ -136,6 +136,7 @@ def create():
     If a completion is created successfully, directs to the main page.
     else, recursively redirect to this page (completion/create) until a successful completion"""
     if request.method == "POST":
+        print("called function create")
         top_p_str = request.form['top_p']
         top_k_str = request.form['top_k']
         num_groups = request.form['num_groups']
@@ -145,22 +146,23 @@ def create():
         num_groups_int = int(num_groups)
         top_p_float = float(top_p_str)
         top_k = int(top_k_str)
-        g.loaded_tokenizers[model_name], _ = init_tf_tokenizer()
         pb_model_path = f"/flaskr/static/models/{model_name}.pb"
         h5_model_path = f"/flaskr/static/models/{model_name}.h5"
 
-        if os.path.exists(pb_model_path):
-            g.loaded_models[model_name] = tf.saved_model.load(pb_model_path)
-            g.loaded_tokenizers[model_name], _ = init_tf_tokenizer()
+        if not model_name in g.loaded_models.keys():
+            if os.path.exists(pb_model_path):
+                g.loaded_models[model_name] = tf.saved_model.load(pb_model_path)
+                g.loaded_tokenizers[model_name], _ = init_tf_tokenizer()
 
-        elif os.path.exists(h5_model_path):
-            g.loaded_models[model_name] = tf.keras.models.load_model(h5_model_path)
-            g.loaded_tokenizers[model_name], _ = init_tf_tokenizer()
+            elif os.path.exists(h5_model_path):
+                g.loaded_models[model_name] = tf.keras.models.load_model(h5_model_path)
+                g.loaded_tokenizers[model_name], _ = init_tf_tokenizer()
 
-        else:
-            g.loaded_models[model_name] = AutoModelForCausalLM.from_pretrained(model_name)
-            g.loaded_tokenizers[model_name] = AutoTokenizer.from_pretrained(model_name)
+            else:
+                g.loaded_models[model_name] = AutoModelForCausalLM.from_pretrained(model_name)
+                g.loaded_tokenizers[model_name] = AutoTokenizer.from_pretrained(model_name)
 
+        print("loaded model and tokenizer")
         completions = complete(model_name, prompt, top_p_float, top_k, num_groups_int, group_size)
         try:
             best_completion = max(completions, key=completions.get)
