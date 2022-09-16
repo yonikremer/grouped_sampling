f1a404b145cca2d57ac9829c5d7a94b0c256ef12
yoni kremer <yoni.kremer@gmail.com>
2022-09-16 18:57:52 +0300

    Supporting only pretrained models

--- web_app/flaskr/completion.py
+++ web_app/flaskr/completion.py
@@ -1,14 +1,12 @@
 """Contains the functions for the completion page and the completion blueprint"""
-import os
 
-import tensorflow as tf
 from flask import Blueprint, g, redirect, render_template, request, url_for, flash
-from transformers import AutoTokenizer, AutoModelForCausalLM
 
+from sample_generator import SampleGenerator
+from tree_generator import TreeGenerator
 from flaskr.auth import login_required
 from flaskr.db import get_db
 from flaskr.tokenizer import init_tf_tokenizer
-from flaskr.sampling import complete
 
 
 bp = Blueprint('completion', __name__)
@@ -43,39 +41,21 @@ def create():
     else, recursively redirect to this page (completion/create) until a successful completion"""
     if request.method == "POST":
         print("called function create")
-        top_p_str = request.form['top_p']
-        top_k_str = request.form['top_k']
-        num_groups = request.form['num_groups']
+        top_p = request.form['top_p']
+        top_k = request.form['top_k']
+        num_tokens = request.form['num_tokens']
         prompt = request.form['prompt']
         model_name = request.form['model_name']
         group_size = request.form['group_size']
-        num_groups_int = int(num_groups)
-        top_p_float = float(top_p_str)
-        top_k = int(top_k_str)
-        pb_model_path = f"/flaskr/static/models/{model_name}.pb"
-        h5_model_path = f"/flaskr/static/models/{model_name}.h5"
-
-        if not model_name in g.loaded_models.keys():
-            if os.path.exists(pb_model_path):
-                g.loaded_models[model_name] = tf.saved_model.load(pb_model_path)
-                g.loaded_tokenizers[model_name], _ = init_tf_tokenizer()
-
-            elif os.path.exists(h5_model_path):
-                g.loaded_models[model_name] = tf.keras.models.load_model(h5_model_path)
-                g.loaded_tokenizers[model_name], _ = init_tf_tokenizer()
-
-            else:
-                g.loaded_models[model_name] = AutoModelForCausalLM.from_pretrained(model_name)
-                g.loaded_tokenizers[model_name] = AutoTokenizer.from_pretrained(model_name)
-
-        print("loaded model and tokenizer")
-        completions = complete(model_name, prompt, top_p_float, top_k, num_groups_int, group_size)
-        try:
-            best_completion = max(completions, key=completions.get)
-        except ValueError:
-            flash("No completions were found, try using higher top-k or top-p or smaller group size")
-            flash("That happend because all the models had repeated tokens")
-            return redirect(url_for('completion/index.html'))
-        add_answer_to_db(model_name, prompt, best_completion)
+        generation_type = request.form['generation_type']
+        temperature = request.form['temperature']
+        
+        add_answer_to_db(model_name, prompt, answer)
+        if generation_type == "tree":
+            generator = TreeGenerator(model_name, group_size, top_k, top_p, temperature)
+        elif generation_type == "sampling":
+            generator = SampleGenerator(model_name, group_size, top_k, top_p, temperature)
+        answer: str = generator(prompt, num_tokens) 
+        add_answer_to_db(model_name, prompt, answer)
         return redirect(url_for('completion/index.html'))
     return render_template("completion/create.html")
--- web_app/flaskr/model.py
+++ web_app/flaskr/model.py
@@ -1,14 +1,10 @@
 """Contains the functions and blueprint to upload a model and see a list of all models"""
 import os
 
-import tensorflow as tf
-import keras
-from flask import Blueprint, render_template, request, flash, redirect, url_for, current_app, g
-from werkzeug.utils import secure_filename
+from flask import Blueprint, render_template, g
 import pandas as pd
 
 from flaskr.db import get_db
-from flaskr.completion import complete
 from flaskr.auth import login_required
 
 bp = Blueprint('model', __name__)
@@ -23,101 +19,9 @@ def view_all():
     return render_template("model/view_all.html", table = [data_frame.to_html()], titles = data_frame.columns.values)
 
 
-def file_is_allowed(filename):
-    """return if the file has the correct extension"""
-    banned_chars = {'/', ':', '*', '?', '<', '>', '|'}
-    has_banned_chars = not any(char in filename for char in banned_chars)
-    is_pb_file = filename.endswith(".pb")
-    return is_pb_file and has_banned_chars
-
-
-def model_errors(model_name):
-    """Returns a list with all the errors when checking the model"""
-    errors = []
-    path = f"/flaskr/static/models/{model_name}.pb"
-    model = tf.saved_model.load(path)
-    if not isinstance(model, keras.Model):
-        return ['The file is not a valid keras.Model']
-    first_layer = model.get_layer(name = None, index = 0)
-    if not isinstance(first_layer, keras.layers.Embedding):
-        errors.append('The first layer of your model is not an Embedding layer')
-    last_layer = model.get_layer(name = None, index = -1)
-    if not isinstance(last_layer, keras.layers.Softmax):
-        errors.append('The last layer of your model is not a Softmax layer')
-    else:
-        if last_layer.axis not in (-1, 2):
-            errors.append('The last layer of your model is not a Softmax layer with axis=-1 or axis=2')
-    test_completion = complete(model, "This is a test", 1.0, 1, 1, 1)
-    if not isinstance(test_completion, str):
-        errors.append('Your model does not generate the correct output. Please try a different model')
-    return errors
-
-
-def type_errors(my_request):
-    """Returns all the type errors messages"""
-    errors = ""
-    arg_names = ("set_size", "num_layers", "num_heads", "d_model", "dff",
-           "dropout_rate", "learning_rate", "batch_size")
-    arg_types = (int, int, int, int, int, float, float, int)
-    for _id, _type in zip(arg_names, arg_types):
-        if _id not in my_request.form:
-            errors += f'Missing {_id}'
-        value = my_request.form[_id]
-        if "e" in value.lower() or "**" in value or "^" in value:
-            errors += 'e is not allowed in numbers, (use 0.01 and not 1e-2, 1E-2, 10^-2, 10**-2)'
-        if str(_type(value)) != value:
-            errors += f'{_id} is not a valid {_type}'
-    return errors
-
-
+@bp.route('/new', methods = ("Post", "GET"))
 @login_required
-@bp.route('/upload', methods = ("POST", "GET"))
-def upload():
-    """A page where every user can upload their own model
-    If uploading is successful, redirect to """
-    if request.method == 'POST':
-        # check if the post request has the file part
-        errors = []
-        if 'file' not in request.files:
-            errors.append('No file part')
-        file = request.files['file']
-        if not file:  # if no file is selected => file is None
-            errors.append('File is None')
-        # if user does not select file, browser also
-        # submit an empty part without model_name
-        if file.filename == '':
-            errors.append('Selected file does not have a name')
-        if not file_is_allowed(file.filename):
-            errors.append('File extension is not .pb')
-        filename = secure_filename(file.filename)
-        model_name = filename.split('.')[0]
-        if len(errors) == 0:
-            full_path = os.path.join(current_app.root_path, "static", "models", filename)
-            file.save(full_path)
-            flash('File successfully uploaded')
-            errors = model_errors(model_name)  # The same as appending to errors because it's empty
-            if len(errors) > 0:
-                os.remove(full_path)
-                errors.append('Model removed')
-
-        set_size = int(request.form["set_size"])
-        num_layers = int(request.form["num_layers"])
-        num_heads = int(request.form["num_heads"])
-        d_model = int(request.form["d_model"])
-        dff = int(request.form["dff"])
-        dropout_rate = float(request.form["dropout_rate"])
-        learning_rate = float(request.form["learning_rate"])
-        batch_size = int(request.form["batch_size"])
-        errors += type_errors(request)
-        if len(errors) == 0:
-            my_db = get_db()
-            my_db.execute("""INSERT INTO model
-                          (user_id, model_name, set_size, num_layers, num_heads, 
-                          d_model, dff, dropout_rate, learning_rate, batch_size)
-                          VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)""",
-                          (g.user["id"], model_name, set_size, num_layers, num_heads, d_model, 
-                          dff, dropout_rate, learning_rate, batch_size))
-            return redirect(url_for("model.view_all"))
-        flash('\n'.join(errors))
-
-    return render_template("model/upload.html")
+def new(model_name):
+    """Upload a new model"""
+    my_db = get_db()
+    my_db.execute("INSERT INTO model (name, user_id) VALUES (?, ?)", (model_name, g.user['id']))
