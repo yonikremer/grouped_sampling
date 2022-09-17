c5f42804ca33da3eff3dc6c080d639628322bcf7
yoni kremer <yoni.kremer@gmail.com>
2022-09-17 11:09:46 +0300

    Created a function to add model to the database.

--- web_app/flaskr/model.py
+++ web_app/flaskr/model.py
@@ -19,9 +19,13 @@ def view_all():
     return render_template("model/view_all.html", table = [data_frame.to_html()], titles = data_frame.columns.values)
 
 
-@bp.route('/new', methods = ("Post", "GET"))
-@login_required
-def new(model_name):
-    """Upload a new model"""
+def add_model_to_db(model_name: str) -> int:
+    """Adds a model to the database if it doesn't exist already and returns the id of the model"""
     my_db = get_db()
-    my_db.execute("INSERT INTO model (name, user_id) VALUES (?, ?)", (model_name, g.user['id']))
+    # Check if the model already exists
+    model_exists: bool = my_db.execute("SELECT EXISTS(SELECT 1 FROM model WHERE name=? LIMIT 1)", (model_name,))
+    if not model_exists:
+        my_db.execute("INSERT INTO model (name, user_id) VALUES (?, ?)", (model_name, g.user['id']))
+    model_id = my_db.execute("SELECT id FROM model WHERE name=?", (model_name,)).fetchone()
+    my_db.commit()
+    return model_id
