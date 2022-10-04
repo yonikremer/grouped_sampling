146f04df02347b0094e4ca80f4bebc22e61c01a9
yoni kremer <yoni.kremer@gmail.com>
2022-10-04 23:09:27 +0300

    Deleted wrong implementation for text generator creating

--- web_app/flaskr/completion.py
+++ web_app/flaskr/completion.py
@@ -2,21 +2,20 @@
 
 from dataclasses import dataclass
 
-from flask import Blueprint, g, redirect, render_template, request, url_for
+from flask import Blueprint, g, render_template, request
 
 from text_generator import TextGenerator
-from sample_generator import SampleGen
-from tree_generator import TreeGen
-from flaskr.auth import login_required
-from flaskr.db import get_db
-from flaskr.model import add_model_to_db
-
+from sampling_generator import SamplingGenerator
+from tree_generator import TreeGenerator
+from .auth import login_required
+from .database import get_db
+from .model import get_model_id
 
 bp = Blueprint('completion', __name__)
 
 
 @dataclass
-class ComplitionData:
+class CompletionData:
     """Contains all the data for the completion page"""
     prompt: str
     answer: str
@@ -31,47 +30,45 @@ def index():
     # Execute a SQL query and return the results
     query = "SELECT * FROM completion c JOIN user u ON c.user_id = u.id ORDER BY created DESC"
     completions = my_db.execute(query).fetchall()
-    return render_template("completion/index.html", completions = completions)
+    return render_template("completion/index.html", completions=completions)
 
 
-def add_comp_to_db(comp_data: ComplitionData):
+def add_comp_to_db(comp_data: CompletionData):
     """Adds an answer to the database"""
     STRUCTURE: str = """INSERT INTO completion 
-                        (user_id, model_id, prompt, answer, num_tokens, generation_type, top_p, top_k, temprature)
+                        (user_id, model_id, prompt, answer, num_tokens, generation_type, TOP_P, TOP_K, temprature)
                         VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)"""
     connection = get_db()
     generator: TextGenerator = comp_data.generator
-    model_id: int = add_model_to_db(generator.model_name)
+    model_id: int = get_model_id(generator.model_name)
     arguments = (g.user['id'], model_id, comp_data.prompt, comp_data.answer,
-                comp_data.num_tokens, generator.generation_type,
-                generator.top_p, generator.top_k,
-                generator.temperature)
+                 comp_data.num_tokens, generator.generation_type,
+                 generator.top_p, generator.top_k,
+                 generator.temperature)
     connection.execute(STRUCTURE, arguments)
     connection.commit()
 
 
 @login_required
-@bp.route('/create', methods = ('GET', 'POST'))
+@bp.route('/create', methods=('GET', 'POST'))
 def create():
     """Creates a new completion
     If a completion is created successfully, directs to the main page.
     else, recursively redirect to this page (completion/create) until a successful completion"""
     if request.method == "POST":
         print("called function create")
-        top_p = request.form['top_p']
-        top_k = request.form['top_k']
+        top_p = request.form['TOP_P']
+        top_k = request.form['TOP_K']
         num_tokens = request.form['num_tokens']
         prompt = request.form['prompt']
         model_name = request.form['model_name']
         group_size = request.form['group_size']
         generation_type = request.form['generation_type']
         temperature = request.form['temperature']
-        if generation_type == "tree":
-            generator = TreeGen(model_name, group_size, top_k, top_p, temperature)
-        elif generation_type == "sampling":
-            generator = SampleGen(model_name, group_size, top_k, top_p, temperature)
-        answer: str = generator(prompt, num_tokens)
-        completion = ComplitionData(prompt, answer, num_tokens, generator) 
-        add_comp_to_db(completion)
-        return redirect(url_for('completion/index.html'))
+        raise NotImplementedError
+        # TODO: create a generator based on the form data
+        # answer: str = generator(prompt, num_tokens)
+        # completion = CompletionData(prompt, answer, num_tokens, generator)
+        # add_comp_to_db(completion)
+        # return redirect(url_for('completion/index.html'))
     return render_template("completion/create.html")
