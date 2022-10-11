472dd86eca63cb5ced278de7f15d378b253b5e09
yoni kremer <yoni.kremer@gmail.com>
2022-10-11 14:57:39 +0300

    Fixed wrong column name error in get_model_id

--- web_app/flaskr/model.py
+++ web_app/flaskr/model.py
@@ -17,13 +17,20 @@ def view_all():
     return render_template("model/view_all.html", table=[data_frame.to_html()], titles=data_frame.columns.values)
 
 
-def get_model_id(model_name: str) -> int:
+def get_model_id(my_model_name: str) -> int:
     """Adds a model to the database if it doesn't exist already and returns the id of the model"""
     my_db = get_db()
     # Check if the model already exists
-    model_exists: bool = my_db.execute("SELECT EXISTS(SELECT 1 FROM model WHERE name=? LIMIT 1)", (model_name,))
+    model_exists: bool = my_db.execute(
+        "SELECT EXISTS(SELECT 1 FROM model WHERE model_name=? LIMIT 1)",
+        (my_model_name,))
     if not model_exists:
-        my_db.execute("INSERT INTO model (name, user_id) VALUES (?, ?)", (model_name, g.user['id']))
-    model_id: int = my_db.execute("SELECT id FROM model WHERE name=?", (model_name,)).fetchone()
+        my_db.execute(
+            "INSERT INTO model (model_name, user_id) VALUES (?, ?)",
+            (my_model_name, g.user['id']))
+    model_id: int = my_db.execute(
+        "SELECT id FROM model WHERE model_name=?",
+        (my_model_name,)
+    ).fetchone()
     my_db.commit()
     return model_id
