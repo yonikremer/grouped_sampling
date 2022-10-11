dd44381ff1e0ca19b82638d35d67bb9c6147d73b
yoni kremer <yoni.kremer@gmail.com>
2022-10-11 17:54:58 +0300

    Added details to user instructions

--- open_mercury_demo.py
+++ open_mercury_demo.py
@@ -8,13 +8,21 @@ except (ImportError, ModuleNotFoundError):
     pip.main(['install', 'mljar-mercury'])
     exit("Please run this script again.")
 
+NOTEBOOK_FULL_PATH = "C:/yoni/final_project/mercury_demo/mercury_demo.ipynb"
+
 
 def main():
     """Opens Mercury demo in your browser."""
     os.chdir(os.path.dirname(os.path.abspath(__file__)))
     url = "http://127.0.0.1:8000/"
+
+    os.system(f"mercury add {NOTEBOOK_FULL_PATH}")
+
     print(f"Please enter this URL in your browser: {url}")
-    os.system("mercury run mercury_demo.ipynb")
+    print("You might need to wait a few seconds for the server to start.")
+    print("Press Ctrl+C (for windows and linux) or command+C (for macOS) to stop the server.")
+
+    os.system(f"mercury run {NOTEBOOK_FULL_PATH}")
 
 
 if __name__ == "__main__":
