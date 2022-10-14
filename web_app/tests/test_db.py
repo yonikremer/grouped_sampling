6541ebe83809be0edbaecd1c12049715eb30ac4c
yoni kremer <yoni.kremer@gmail.com>
2022-10-14 15:49:50 +0300

    Added a test to check if the database is empty

--- web_app/tests/test_db.py
+++ web_app/tests/test_db.py
@@ -1,6 +1,8 @@
 """This file contains all the tests for the database."""
 from sqlite3 import Connection
 
+from pytest import mark
+
 from web_app.flaskr.database import get_db
 from flask import Flask
 
@@ -11,3 +13,16 @@ def test_and_get_close_db(app: Flask):
         my_db: Connection = get_db(testing=True)
         assert my_db is get_db(testing=True)  # Assert that get_db()
         # returns a pointer to the same database every time
+
+
+@mark.parametrize(
+    "table_name",
+    ("user", "model", "completion"),
+)
+def test_tables_are_not_empty(app: Flask, table_name: str):
+    """Test that the tables of the database are not empty."""
+    with app.app_context():
+        my_db: Connection = get_db(testing=True)
+        my_cursor = my_db.cursor()
+        my_cursor.execute(f"SELECT * FROM {table_name}")
+        assert my_cursor.fetchone()
