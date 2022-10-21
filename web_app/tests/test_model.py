17ccd248c0a58eeb2f402e507572cb40ccaf240e
yoni kremer <yoni.kremer@gmail.com>
2022-10-21 17:40:40 +0300

    reimplemented testing for the model blueprint

--- web_app/tests/test_model.py
+++ web_app/tests/test_model.py
@@ -1,52 +1,24 @@
 """Contains the tests for the model blueprint."""
 from typing import List
+
+from flask import Flask
 from flask.testing import FlaskClient
 
 from web_app.tests.conftest import AuthActions
+from web_app.tests.test_base_html import test_base_html
 
 
-def test_view_all(client: FlaskClient, auth: AuthActions) -> None:
+def test_view_all(app: Flask, client: FlaskClient, auth: AuthActions) -> None:
     """Tests that the view_all function returns the correct data."""
-    response = client.get('model/view_all')
-    assert response.status_code == 200
-    # Assert that the view_all.html extends from base.html.
-    assert b'Log In' in response.data
-    assert b'Register' in response.data
-    assert b'Log Out' not in response.data
-
-    auth.login()
-    assert b'Log In' not in response.data
-    assert b'Register' not in response.data
+    test_base_html(client, auth, "/view_all")
 
     components: List[str] = [
         "View All Models",
-        "Upload your own models!",
-        "user_id",
+        "username",
         "created",
         "model_name",
-        "set_size",
-        "num_layers",
-        "d_model",
-        "dff",
-        "num_heads",
-        "dropout_rate",
-        "learning_rate",
-        "batch_size",
-        "id",
-        "128",
-        "64",
-        "0.1",
-        'Log Out']
+        ]
+    response = client.get("/view_all")
 
     for component in components:
-        assert bytes(component) in response.data
-
-
-def test_model_errors(client: FlaskClient, auth: AuthActions) -> None:
-    """Tests the model errors function"""
-    raise NotImplementedError
-
-
-def test_upload():
-    """Tests the uploading function"""
-    raise NotImplementedError
+        assert component in response.get_data(as_text=True)
