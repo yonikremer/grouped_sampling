4c858e81802ef14a897e44712a3c488a91127a19
yoni kremer <yoni.kremer@gmail.com>
2022-11-05 21:17:47 +0200

    The evaluation can be run on kaggle notebooks too

--- evaluation/experiment_manager.py
+++ evaluation/experiment_manager.py
@@ -4,6 +4,14 @@ from typing import List, Dict
 
 from comet_ml import Experiment
 
+try:
+    # noinspection PyUnresolvedReferences
+    from kaggle_secrets import UserSecretsClient
+except ImportError:
+    using_kaggle = False
+else:  # if we are using kaggle, we need to set the api key
+    using_kaggle = True
+
 from text_generator import TextGenerator
 
 
@@ -34,6 +42,8 @@ class ExperimentManager:
     def get_comet_api_key() -> str:
         """Returns the Comet API key from the file "final_project/evaluate/comet_api_key.txt"
         if this file does not exist, asks the user to enter the key manually and saves it to the file"""
+        if using_kaggle:
+            return UserSecretsClient().get_secret("comet_api_key")
         if os.getcwd() == "/content":
             # if running on colab
             api_key_file = "final_project/evaluation/comet_ml_api_key.txt"
