bd92b1a066392eb285a1b2866915ab72575154f6
yoni kremer <yoni.kremer@gmail.com>
2022-11-05 21:27:43 +0200

    Bug fix: wrong name for user secret

--- evaluation/experiment_manager.py
+++ evaluation/experiment_manager.py
@@ -43,7 +43,7 @@ class ExperimentManager:
         """Returns the Comet API key from the file "final_project/evaluate/comet_api_key.txt"
         if this file does not exist, asks the user to enter the key manually and saves it to the file"""
         if using_kaggle:
-            return UserSecretsClient().get_secret("comet_api_key")
+            return UserSecretsClient().get_secret("comet_ml_api_key")
         if os.getcwd() == "/content":
             # if running on colab
             api_key_file = "final_project/evaluation/comet_ml_api_key.txt"
