78e6d7add3c5b56ee0707c8983321e6ce8663079
yoni kremer <yoni.kremer@gmail.com>
2022-11-05 12:25:29 +0200

    Added type hints and a doctoring

--- evaluation/experiment_manager.py
+++ evaluation/experiment_manager.py
@@ -1,8 +1,9 @@
 import os
 from datetime import datetime
-from typing import List
+from typing import List, Dict
 
 from comet_ml import Experiment
+from torch import Tensor
 
 from text_generator import TextGenerator
 
@@ -32,6 +33,8 @@ class ExperimentManager:
 
     @staticmethod
     def get_comet_api_key() -> str:
+        """Returns the Comet API key from the file "final_project/evaluate/comet_api_key.txt"
+        if this file does not exist, asks the user to enter the key manually and saves it to the file"""
         if os.getcwd() == "/content":
             # if running on colab
             api_key_file = "final_project/evaluation/comet_ml_api_key.txt"
@@ -47,7 +50,11 @@ class ExperimentManager:
                 f.write(COMET_ML_API_KEY)
         return COMET_ML_API_KEY
 
-    def log_sub_experiment(self, bert_scores) -> None:
+    def log_sub_experiment(self, bert_scores: Dict[str, Tensor]) -> None:
+        """Args:
+            bert_scores: Dict[str, Tensor]
+                with keys "f1", "precision", "recall"
+                values of shape (number of examples in the sub-experiment,) and type float"""
         self.per_example_f1.extend(bert_scores["f1"].tolist())
         self.per_example_precision.extend(bert_scores["precision"].tolist())
         self.per_example_recall.extend(bert_scores["recall"].tolist())
