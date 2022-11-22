92830cedd951fb98277c51a3ef9a2a62ccdf47bf
yoni kremer <yoni.kremer@gmail.com>
2022-11-22 13:55:17 +0200

    Fixed typo

--- evaluation/run_evaluation_remotely_script.sh
+++ evaluation/run_evaluation_remotely_script.sh
@@ -2,4 +2,4 @@ rm -rf ./final_project
 git clone --quiet https://github.com/yonikremer/final_project.git
 pip install --quiet --requirement ./final_project/evaluation/evaluation_requirements.txt
 pip install --quiet --requirement ./final_project/project_requirements.txt
-export PYTHONPATH="${PYTHONPATH}:/kaggle/working/final_project" && python -o ./final_project/evaluation/evaluate_translation.py
+export PYTHONPATH="${PYTHONPATH}:/kaggle/working/final_project" && python -O ./final_project/evaluation/evaluate_translation.py
