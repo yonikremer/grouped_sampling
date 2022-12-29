ae42b701346ce6c84d7b25e328c6eca59f9db974
yoni kremer <yoni.kremer@gmail.com>
2022-12-29 18:13:53 +0200

    Simplified generation_type

--- text_generator.py
+++ text_generator.py
@@ -1,39 +1,21 @@
 from __future__ import annotations
 
-import timeit
-from enum import Enum
 from abc import ABC, abstractmethod
 from collections.abc import Callable
 from typing import Optional, List, Union, Dict, Tuple, Any
-from warnings import warn
 
-from torch import LongTensor, ones, cuda, tensor, no_grad, Tensor, cat, long
-from torch.nn import Softmax
+from torch import LongTensor, tensor, cat
 from transformers import (AutoTokenizer,
-                          AutoModelForCausalLM,
                           AutoConfig,
                           BatchEncoding,
                           PreTrainedTokenizer,
-                          PreTrainedModel)
+                          )
 from transformers.tokenization_utils_base import TruncationStrategy
 
-TokenIDS = Union[List[int], Tuple[int]]
-CompletionDict = Dict[str, Union[str, LongTensor]]
+from model_wrapper import ModelWrapper
+from __init__ import TokenIDS, GenerationType, CompletionDict
+
 MAX_MODEL_INPUT_SIZE = 8192
-dim_one_softmax = Softmax(dim=1)
-
-
-class GenerationType(Enum):
-    """The type of generation to use"""
-    GREEDY = "greedy"
-    TOP_K = "top_k"
-    TOP_P = "top_p"
-    TREE = "tree"
-    RANDOM = "random"
-
-    def requires_softmax(self) -> bool:
-        """Whether the generation type requires a softmax"""
-        return self in (self.TOP_K, self.TOP_P, self.TREE, self.RANDOM)
 
 
 class TextGenerator(Callable, ABC):
@@ -47,7 +29,7 @@ class TextGenerator(Callable, ABC):
 
     model_name: str
     tokenizer: PreTrainedTokenizer
-    model: PreTrainedModel
+    wrapped_model: ModelWrapper
     vocab_size: int
     temp: float
     group_size: int
@@ -61,19 +43,17 @@ class TextGenerator(Callable, ABC):
     repetition_penalty_theta: float = 1.2
     descriptive_attrs = (
         "model_name",
-        "group_size",
         "generation_type",
-        "temp",
-        "end_of_sentence_stop",
         "answer_length_multiplier",
-        "repetition_penalty_theta",
+        "wrapped_model",
     )
 
     def __init__(self, model_name: str, group_size: int,
-                 temp: float = 1.0,
-                 end_of_sentence_stop: bool = False,
+                 temp: float,
+                 end_of_sentence_stop: bool,
+                 repetition_penalty_theta: float,
                  answer_length_multiplier: float = 16,
-                 repetition_penalty_theta: float = 1.2, ):
+                 ):
         """Model name: the name of the model
         used for loading from hugging face hub
         group size: int
@@ -90,33 +70,30 @@ class TextGenerator(Callable, ABC):
         self.model_name = model_name
         self.tokenizer = AutoTokenizer.from_pretrained(
             model_name)
-        try:
-            self.model = AutoModelForCausalLM.from_pretrained(
-                model_name)
-        except RuntimeError as e:
-            raise RuntimeError(f"Failed to execute model loading,"
-                               f"Using the AutoModelForCausalLM.from_pretrained({model_name}) command") from e
-        if cuda.is_available():
-            self.model = self.model.cuda()
-        else:
-            warn("CUDA is not available, using CPU")
-        self.vocab_size = self.tokenizer.vocab_size
-        self.temp = temp
-        self.group_size = group_size
-        self.end_of_sentence_id = self.tokenizer.eos_token_id
-        self.end_of_sentence_stop = end_of_sentence_stop and self.end_of_sentence_id is not None
-        self.max_input_len = self.tokenizer.model_max_length
-        max_len_is_huge = self.max_input_len > MAX_MODEL_INPUT_SIZE
-        if max_len_is_huge or self.max_input_len is None:
+        end_of_sentence_id = self.tokenizer.eos_token_id
+        end_of_sentence_stop = end_of_sentence_stop and end_of_sentence_id is not None
+        max_input_len = self.tokenizer.model_max_length
+        max_len_is_huge = max_input_len > MAX_MODEL_INPUT_SIZE
+        if max_len_is_huge or max_input_len is None:
             config = AutoConfig.from_pretrained(model_name)
-            self.max_input_len = config.max_position_embeddings
-            max_len_is_still_huge = self.max_input_len > MAX_MODEL_INPUT_SIZE
-            if max_len_is_still_huge or self.max_input_len is None:
+            max_input_len = config.max_position_embeddings
+            max_len_is_still_huge = max_input_len > MAX_MODEL_INPUT_SIZE
+            if max_len_is_still_huge or max_input_len is None:
                 raise ValueError(
                     "The maximum length of the model is too big"
                 )
+        self.wrapped_model = ModelWrapper(
+            model_name=model_name,
+            group_size=group_size,
+            max_input_len=max_input_len,
+            end_of_sentence_id=end_of_sentence_id,
+            end_of_sentence_stop=end_of_sentence_stop,
+            repetition_penalty_theta=repetition_penalty_theta,
+            padding_id=self.tokenizer.pad_token_id,
+            temp=temp,
+            use_softmax=self.generation_type.requires_softmax(),
+        )
         self.answer_length_multiplier = answer_length_multiplier
-        self.repetition_penalty_theta = repetition_penalty_theta
 
     @property
     @abstractmethod
@@ -125,125 +102,6 @@ class TextGenerator(Callable, ABC):
         Returns:
             a GenerationType object"""
         raise NotImplementedError
-
-    @property
-    def padding_tokens(self) -> LongTensor:
-        pad_id = self.tokenizer.pad_token_id
-        if pad_id is None:
-            pad_id = 0
-        return ones(self.group_size, dtype=long) * pad_id
-
-    def penalize_logits(self, logits: Tensor, tokens: TokenIDS, generation_start: int) -> Tensor:
-        """applies repetition penalty,
-        using the repetition_penalty_theta parameter defined in the class
-        the formula from the paper is:
-        softmax(original_logits, T) = exp(original_logits_i/(T · h(i)) / sum(exp(original_logits_i/(T · h(i)))
-        which is equivalent to:
-        pi = exp((xi/h(i)) / T / sum(exp(xj/(T · h(j)))
-        and to:
-        penalized_logits = original_logits / h(i)
-        pi = softmax(original_logits / h(i), T)
-        where:
-            h(i) = θ if i in generated_tokens else 1
-            T is the temperature parameter of softmax
-        Args:
-            logits: the logits matrix of shape (group_size, vocab_size)
-            tokens: the sequence of tokes from the prompt and the generated
-            generation_start: The index of the first
-        Returns:
-            original_logits / h(i)
-        complexity: O(group_size * n) where n is the number of tokens generated by the algorithm
-        """
-        if self.repetition_penalty_theta == 1.0:
-            # There is no point diving by zero so in this case there is no need for the function.
-            return logits
-        generated_tokens = set(tokens[generation_start:])  # tokens[generation_start:] has maximum size of n
-        for token_id in generated_tokens:  # len(generated_tokens) < max(n, vocab_size)
-            logits[:, token_id] /= self.repetition_penalty_theta
-            # O(group_size) because we are taking a slice of size group_size
-        return logits
-
-    def prepare_model_kwargs(self, tokens: TokenIDS) -> Dict[str, LongTensor]:
-        """preparing the arguments for the model call
-        Args:
-            tokens: the tokens to be sent to the model
-        Returns:
-            a dictionary of the arguments for the model call
-        Complexity: O(group_size + n) where n is the number of tokens
-        """
-        if not isinstance(tokens, Tensor):
-            tokens = LongTensor(tokens)  # O(n)
-        padded_tokens: LongTensor = cat((tokens, self.padding_tokens), dim=0)
-        # the length of padded_tokens is n + group_size - 1
-        # so creating it is O(n + group_size)
-        attention_len = len(padded_tokens)  # n + group_size - 1
-        if attention_len > self.max_input_len:
-            padded_tokens = padded_tokens[-self.max_input_len:]
-            # O(self.max_input_len) which is constant so O(1)
-        attention_mask: LongTensor = ones([1, attention_len], dtype=long)
-        # O(attention_len) so O(n + group_size)
-        if cuda.is_available():
-            padded_tokens = padded_tokens.cuda()  # O(n + group_size)
-            attention_mask = attention_mask.cuda()  # O(n + group_size)
-        else:
-            warn("CUDA is not available, using CPU")
-        return {
-            "input_ids": padded_tokens.unsqueeze(0),
-            "attention_mask": attention_mask,
-        }
-
-    def get_logits_matrix(self, tokens: TokenIDS) -> Tensor:
-        """Given a sequence of tokens,
-        returns the logits matrix of shape (group_size, vocab_size)
-        where logits[i] is the logits vector of the i-th next token
-        complexity: O(n^2 + group_size^2) where n is the length of the tokens
-        notice that the logits are not divided by the temperature in this function."""
-        # define n as the number of tokens in tokens
-        model_kwargs = self.prepare_model_kwargs(tokens)  # O(n + group_size)
-        with no_grad():
-            # The time complexity of causal language model`s __call__ function
-            # is O(n^2) where n is the length of the inputs
-            outputs = self.model(
-                **model_kwargs
-            )
-            # the length of all the inputs is n + group_size - 1
-            # so the complexity of this line is O((n + group_size - 1)^2)
-            # which is O(n^2 + group_size^2 + group_size * n)
-            # we now that if a > b and a, b > 1 then a^2 > ab so the complexity is O(n^2 + group_size^2)
-        unscaled_relevant_logits: Tensor = outputs.logits[0, -self.group_size:, :self.vocab_size]
-        # The shape of unscaled_relevant_logits is (group_size, vocab_size)
-        # So the complexity of this line should be O(group_size) because we are coping group_size * vocab_size
-        # elements from one tensor to the another
-        return unscaled_relevant_logits
-
-    def get_prob_mat(self, tokens: TokenIDS, generation_start: int) -> Tensor:
-        """Returns the probability matrix
-         as a list of lists of floats
-         Time complexity: O(n^2 + group_size^2)
-         where n is the number of tokens"""
-        unscaled_relevant_logits = self.get_logits_matrix(tokens)  # O(n^2 + group_size^2)
-        # unscaled_relevant_logits is a tensor of shape (group_size, vocab_size)
-        if not self.end_of_sentence_stop:
-            unscaled_relevant_logits[:, self.end_of_sentence_id] = -float('inf')
-            # setting a vector of size vocab_size so the complexity is O(group_size)
-            # setting the logits to -inf so the probability will be 0
-        penalized_logits = self.penalize_logits(unscaled_relevant_logits, tokens, generation_start)
-        # O(group_size * n) where n is the number of tokens generated by the algorithm
-        if self.generation_type.requires_softmax():
-            # if the generation type is not greedy then we need to apply softmax
-            # to the penalized logits
-            if self.temp != 1.0:
-                penalized_logits /= self.temp
-                # O(group_size * vocab_size) because we are dividing a matrix of size (group_size, vocab_size)
-            prob_tensor: Tensor = dim_one_softmax(penalized_logits)
-        else:
-            # if the generation type is greedy then we don't need to apply softmax
-            # to the penalized logits
-            prob_tensor: Tensor = penalized_logits
-        # We are doing a softmax operator of group_size different vectors of size vocab_size
-        # The complexity of the softmax for each vector is O(1) because the size of the vector is constant
-        # the complexity of this line is O(group_size) because we are doing group_size softmax operations
-        return prob_tensor
 
     def get_token_tensor(
             self,
@@ -512,47 +370,3 @@ class TextGenerator(Callable, ABC):
         if "generation_type" in my_dict.keys():
             my_dict.pop("generation_type")
         return cls(**my_dict)
-
-
-class NoCompletionsFound(Exception):
-    def __init__(
-            self,
-            text_generator: TextGenerator,
-            additional_info: str = ""):
-        super(NoCompletionsFound, self).__init__(
-            f"text generator: {text_generator} \n"
-            f"additional info: {additional_info}")
-
-
-def compare_generators(
-        non_grouped_generator: TextGenerator,
-        prompt: str,
-        num_tokens: int,
-        group_size: int):
-    """Compares grouped and non-grouped text generators"""
-    print(f"Your prompt:")
-    print(prompt)
-
-    start_non_grouped = timeit.default_timer()
-    non_grouped_ans: str = non_grouped_generator(
-        prompt_s=prompt,
-        max_new_tokens=num_tokens
-    )["generated_text"]
-    stop_non_grouped = timeit.default_timer()
-    non_grouped_time = stop_non_grouped - start_non_grouped
-    print(f"Text generated by Non grouped sampling"
-          f" in {non_grouped_time} seconds:")
-    print(non_grouped_ans)
-
-    non_grouped_generator.group_size = group_size
-    grouped_generator = non_grouped_generator
-    start_grouped_generation = timeit.default_timer()
-    grouped_ans: str = grouped_generator(
-        prompt_s=prompt,
-        max_new_tokens=num_tokens
-    )["generated_text"]
-    stop_grouped_generation = timeit.default_timer()
-    grouped_time = stop_grouped_generation - start_grouped_generation
-    print(f"Text generated by grouped sampling"
-          f" in {grouped_time} seconds:")
-    print(grouped_ans)
