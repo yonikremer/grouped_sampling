c60896f1b85a64f574bce1468e4b548336f3afc2
yoni kremer <yoni.kremer@gmail.com>
2023-01-10 17:14:48 +0200

    reverted commit: removed imports of internal functions and methods

--- src/grouped_sampling/__init__.py
+++ src/grouped_sampling/__init__.py
@@ -1,4 +1,13 @@
+from .completion_dict import CompletionDict
+from .token_ids import TokenIDS
 from .generation_type import GenerationType
+from .generation_utils import GroupedGenerationUtils
+from .repetition_penalty import (
+    RepetitionPenaltyStrategy,
+    LogitScalingRepetitionPenalty,
+    NoRepetitionPenalty,
+    DEFAULT_REPETITION_PENALTY,
+)
 from .base_pipeline import GroupedGenerationPipeLine
 from .sampling_pipeline import GroupedSamplingPipeLine
 from .tree_pipeline import GroupedTreePipeLine
