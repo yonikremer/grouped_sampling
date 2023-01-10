faf3fbdcf143b557a935700c4621406e1265998e
yoni kremer <yoni.kremer@gmail.com>
2023-01-10 16:57:35 +0200

    extracted methods to separate classes

--- src/grouped_sampling/base_pipeline.py
+++ src/grouped_sampling/base_pipeline.py
@@ -2,18 +2,19 @@ from __future__ import annotations
 
 from abc import ABC, abstractmethod
 from collections.abc import Callable
-from typing import Optional, List, Union, Dict, Tuple, Any
+from typing import Optional, List, Union, Dict, Any
 
-from torch import LongTensor, tensor, cat
+from torch import LongTensor
 from transformers import (
     AutoTokenizer,
     AutoConfig,
-    BatchEncoding,
 )
 from transformers.tokenization_utils_base import TruncationStrategy
 
 from .generation_type import GenerationType
 from .generation_utils import GroupedGenerationUtils
+from .postproccesor import PostProcessor
+from .preproccesor import PreProcessor
 from .repetition_penalty import RepetitionPenaltyStrategy, DEFAULT_REPETITION_PENALTY
 from .completion_dict import CompletionDict
 from .token_ids import TokenIDS
@@ -79,6 +80,13 @@ class GroupedGenerationPipeLine(Callable, ABC):
                 raise ValueError(
                     "The maximum length of the model is too big"
                 )
+        self.preprocessor = PreProcessor(
+            tokenizer=self.tokenizer,
+            max_input_len=max_input_len,
+        )
+        self.postprocessor = PostProcessor(
+            tokenizer=self.tokenizer,
+        )
         wrapped_model_kwargs: Dict[str, Any] = {
             "model_name": model_name,
             "group_size": group_size,
@@ -115,77 +123,6 @@ class GroupedGenerationPipeLine(Callable, ABC):
             a GenerationType object"""
         raise NotImplementedError
 
-    def get_token_tensor(
-            self,
-            text: str,
-            truncation: TruncationStrategy = TruncationStrategy.DO_NOT_TRUNCATE,
-    ) -> LongTensor:
-        """Complexity: O(n) where n is the number of characters in the text"""
-        if len(text) == 0:
-            return LongTensor([])
-        # tokenizing a string is O(n) where n is the length of the string
-        tokenized_text = self.tokenizer(
-            text,
-            return_tensors=self.framework,
-            padding=False,
-            add_special_tokens=False,
-            truncation=truncation,
-            max_length=self.wrapped_model.max_input_len,
-        )
-        if isinstance(tokenized_text, dict) \
-                or isinstance(tokenized_text, BatchEncoding):
-            token_tensor: LongTensor = tokenized_text["input_ids"]
-            # O(1) because we are accessing a single element
-            # in a dictionary and saving the reference to it.
-        elif isinstance(tokenized_text, LongTensor):
-            token_tensor: LongTensor = tokenized_text
-            # O(1) because we are saving the reference to the tensor
-        else:
-            raise TypeError(
-                "The tokenizer output is not one of:"
-                "dict, BatchEncoding, LongTensor"
-            )
-        token_tensor = token_tensor.squeeze()
-        # O(n) where n is the number of tokens in the text
-        # because we are copying n elements from one tensor to the other
-        # the number of tokens in the text
-        # is always less than the number of characters in the text
-
-        return token_tensor
-
-    def preprocess(
-            self,
-            prompt: str,
-            prefix: str = "",
-            truncation: TruncationStrategy = TruncationStrategy.DO_NOT_TRUNCATE,
-            postfix: str = "",
-    ) -> Tuple[LongTensor, int, int, int]:
-        """A helper method for __call__ that tokenize the prompt
-        all the arguments are sent directly from the __call__ method
-        Returns:
-            the tokenized prompt as a list of ints,
-             the length of the prompt,
-             the length of the prefix,
-             the length of the postfix
-
-        Complexity: O(a + b + c) where:
-            'a' is the number of characters in the prefix
-            'b' is the number of characters in the prompt
-            'c' is the number of characters in the postfix"""
-
-        prefix_tokens: LongTensor = self.get_token_tensor(prefix, truncation)
-        # O(A) where A is the number of characters in the prefix.
-        postfix_tokens: LongTensor = self.get_token_tensor(postfix, truncation)
-        # O(B) where B is the number of characters in the postfix.
-        prompt_tokens: LongTensor = self.get_token_tensor(prompt, truncation)
-        # O(C) where C is the number of characters in the prompt.
-        token_tensor: LongTensor = cat((prefix_tokens, prompt_tokens, postfix_tokens))
-        # O( + b + c) where 'a' is the number of tokens in the prefix.
-        # 'b' is the number of tokens in the prompt.
-        # 'c' is the number of tokens in the postfix.
-        # we know that the number of tokens is less than the number of characters
-        return token_tensor, len(prefix_tokens), len(prompt_tokens), len(postfix_tokens)
-
     @abstractmethod
     def _forward(
             self,
@@ -203,76 +140,6 @@ class GroupedGenerationPipeLine(Callable, ABC):
         Returns:
             the prompt + generated text as a list/tuple of ints"""
         pass
-
-    def postprocess(
-            self,
-            token_ids: TokenIDS,
-            num_new_tokens: Optional[int],
-            prompt_len: int,
-            return_text: bool,
-            return_tensors: bool,
-            return_full_text: bool,
-            clean_up_tokenization_spaces: bool,
-            prefix_len: int = 0,
-            postfix_len: int = 0,
-    ):
-        """A helper method for __call__
-        that converts the token ids to dictionary
-        token_ids - the token ids from the _forward method
-        prompt_len - the length of the tokenized prompt
-        the rest of the arguments are the arguments
-        from the __call__ method
-        look up the documentation of the __call__ method for more info
-        Complexity: O(n) where n is the number of tokens in token_ids
-        """
-        # define n as the length of the token_ids
-        full_prompt_len = prefix_len + prompt_len + postfix_len
-        if num_new_tokens is None:
-            shorten_token_list = token_ids
-            # O(1)
-        else:
-            final_num_tokens = full_prompt_len + num_new_tokens
-            # O(1)
-            if len(token_ids) > final_num_tokens:
-                shorten_token_list = token_ids[:final_num_tokens]
-                # O(final_num_tokens)
-                # because we are copying final_num_tokens
-                # elements from one list to the other
-            else:
-                shorten_token_list = token_ids
-                # O(1)
-
-        generated_tokens = shorten_token_list[full_prompt_len:]
-        # O(num_new_tokens) because we are copying
-        # num_new_tokens elements from one list to the other
-        if return_full_text:
-            prompt_tokens = shorten_token_list[prefix_len:prefix_len + prompt_len]
-            # Without prefix and postfix
-            # O(prompt_len) because we are copying prompt_len
-            # elements from one list to the other
-            final_token_list = prompt_tokens + generated_tokens
-            # O(prompt_len + num_new_tokens)
-            # because we are copying elements from one list to the other
-        else:
-            final_token_list = generated_tokens
-            # O(1) because we are saving the reference to the list
-        final_ans = {}
-        if return_tensors:
-            final_ans["generated_token_ids"] = tensor(final_token_list)
-            # O(prompt_len + num_new_tokens)
-            # because we are copying at maximum
-            # prompt_len + num_new_tokens elements from a list to a tensor
-        if return_text:
-            final_ans["generated_text"] = self.tokenizer.decode(
-                final_token_list,
-                skip_special_tokens=True,
-                clean_up_tokenization_spaces=clean_up_tokenization_spaces
-            )
-            # decoding is O(m)
-            # where m is the number of tokens in the text
-            # so the complexity of this line is O(n)
-            # because final_token_list could be at most n tokens long
-        return final_ans
 
     def __call__(
             self,
@@ -351,7 +218,7 @@ class GroupedGenerationPipeLine(Callable, ABC):
         postfix_len: int
         prompt_len: int
 
-        tokens, prefix_len, prompt_len, postfix_len = self.preprocess(
+        tokens, prefix_len, prompt_len, postfix_len = self.preprocessor(
             prompt=prompt_s,
             prefix=prefix,
             truncation=truncation,
@@ -371,7 +238,7 @@ class GroupedGenerationPipeLine(Callable, ABC):
 
         if num_return_sequences > 1:
             # O(sum(len(tokenized_answer) for tokenized_answer in tokenized_answers))
-            return [self.postprocess(
+            return [self.postprocessor(
                 token_ids=tokenized_answer,
                 num_new_tokens=max_new_tokens,
                 prompt_len=prompt_len,
@@ -384,7 +251,7 @@ class GroupedGenerationPipeLine(Callable, ABC):
             ) for tokenized_answer in tokenized_answers]
         else:
             # O(len(tokenized_answers[0]))
-            return self.postprocess(
+            return self.postprocessor(
                 token_ids=tokenized_answers[0],
                 num_new_tokens=max_new_tokens,
                 prompt_len=prompt_len,
