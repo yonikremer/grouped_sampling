5a9e095846e05bd61cc0bfd3e9187cf504b56781
yoni kremer <yoni.kremer@gmail.com>
2023-01-10 17:23:07 +0200

    Bug fix: attr error - I tried to access a non-existing attr

--- src/grouped_sampling/sampling_pipeline.py
+++ src/grouped_sampling/sampling_pipeline.py
@@ -98,7 +98,7 @@ class GroupedSamplingPipeLine(GroupedGenerationPipeLine):
         if top_k == 1 or top_p == 0.0:
             return GenerationType.GREEDY
         if top_k is not None:
-            if top_k < self.tokenizer.vocab_size:
+            if top_k < self.wrapped_model.vocab_size:
                 return GenerationType.TOP_K
             return GenerationType.RANDOM
         if top_p is not None:
