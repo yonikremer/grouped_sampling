156a7b866b459f8b78bd6e71bea81f7852a0df20
yoni kremer <yoni.kremer@gmail.com>
2023-01-21 17:56:41 +0200

    bug fixes

--- src/grouped_sampling/generation_utils.py
+++ src/grouped_sampling/generation_utils.py
@@ -44,7 +44,7 @@ class GroupedGenerationUtils:
             model_name: the name of the model to be used
             repetition_penalty_strategy:
             the strategy to be used for the repetition penalty
-            group_size: the number of next tokens to be generated
+            group_size: the number of next batch to be generated
             max_input_len: the maximum target_length of the input to the model
             padding_id: the id of the padding token
             vocab_size: the size of the vocabulary
@@ -85,10 +85,10 @@ class GroupedGenerationUtils:
     ) -> Dict[str, LongTensor]:
         """preparing the arguments for the model call
         Args:
-            tokens: the tokens to be sent to the model
+            tokens: the batch to be sent to the model
         Returns:
             a dictionary of the arguments for the model call
-        Complexity: O(group_size + n) where n is the number of tokens
+        Complexity: O(group_size + n) where n is the number of batch
         """
         if not isinstance(tokens, Tensor):
             tokens = LongTensor(tokens)  # O(n)
@@ -119,12 +119,12 @@ class GroupedGenerationUtils:
         }
 
     def get_logits_matrix(self, tokens: TokenIDS) -> Tensor:
-        """Given a sequence of tokens,
+        """Given a sequence of batch,
         returns the logits matrix of shape (group_size, vocab_size)
         where logits[i] is the logits vector of the i-th next token
-        complexity: O(n^2 + group_size^2) where n is the target_length of the tokens
+        complexity: O(n^2 + group_size^2) where n is the target_length of the batch
         notice that the logits are not divided by the temperature in this function."""
-        # define n as the number of tokens in tokens
+        # define n as the number of batch in batch
         model_kwargs = self.prepare_model_kwargs(tokens)  # O(n + group_size)
         with no_grad():
             # The time complexity of causal language model`s __call__ function
@@ -161,9 +161,9 @@ class GroupedGenerationUtils:
         return cat((LongTensor(sequence), padding_tensor), dim=0)
 
     def pad_batch(self, batch: List[TokenIDS], length: int) -> LongTensor:
-        """pads a batch of tokens
+        """pads a batch of batch
         Args:
-            batch: a list of tokens
+            batch: a list of batch
             length: the target_length to pad to
         Returns:
             the padded batch as a LongTensor of size (batch_size, target_length)
@@ -173,7 +173,9 @@ class GroupedGenerationUtils:
             self.pad_sequence(sequence=sequence, target_length=length).unsqueeze(0)
             for sequence in batch
         ]
-        return safe_cat_batch(padded_batch)
+        answer = safe_cat_batch(padded_batch)
+        # assert answer.shape == (len(batch), length), f"{answer.shape} != {(len(batch), length)}"
+        return answer
 
     def prepare_model_kwargs_batch(self, batch: List[TokenIDS]) -> Dict[str, LongTensor]:
         """preparing the arguments for the model call
@@ -181,10 +183,14 @@ class GroupedGenerationUtils:
             batch: the raw batch
         Returns:
             a dictionary of the arguments for the model call
-            Complexity: O(batch size *(group_size + n)) where n is the number of tokens
+            Complexity: O(batch size *(group_size + n)) where n is the number of batch
         """
+        # assert len(batch) > 0, "batch is empty"
+        # assert len(batch) > 1, "use prepare_model_kwargs instead"
         padding_length = max([len(sequence) for sequence in batch]) + self.group_size - 1
         cpu_padded_tokens = self.pad_batch(batch, padding_length)
+        # assert cpu_padded_tokens.shape[1] == padding_length, f"{cpu_padded_tokens.shape[1]} != {padding_length}"
+        # assert cpu_padded_tokens.shape[0] == len(batch), f"{cpu_padded_tokens.shape[0]} != {len(batch)}"
         cpu_attention_mask = ones([len(batch), padding_length], dtype=long)
         if cuda.is_available():
             padded_tokens = cpu_padded_tokens.cuda()
@@ -193,39 +199,73 @@ class GroupedGenerationUtils:
             warn("CUDA is not available, using CPU")
             padded_tokens = cpu_padded_tokens
             attention_mask = cpu_attention_mask
+        # assert padded_tokens.shape[1] == padding_length, f"{padded_tokens.shape[1]} != {padding_length}"
+        # assert padded_tokens.shape[0] == len(batch), f"{padded_tokens.shape[0]} != {len(batch)}"
         return {
             "input_ids": padded_tokens,
             "attention_mask": attention_mask
         }
 
-    def get_logit_mat_batch(self, tokens: List[TokenIDS]) -> Tensor:
+    def get_logit_mat_batch(self, batch: List[TokenIDS]) -> Tensor:
         """Given a batch of sequences of tokens,
         returns the logits matrix of shape (group_size, vocab_size)
         where logits[i] is the logits vector of the i-th next token
-        complexity: O(n^2 + group_size^2) where n is the target_length of the tokens
+        complexity: O(n^2 + group_size^2) where n is the target_length of the batch
         notice that the logits are not divided by the temperature in this function."""
-        # define n as the number of tokens in tokens
-        model_kwargs = self.prepare_model_kwargs_batch(tokens)
+        # define n as the number of batch in batch
+        # assert len(batch) > 0, "batch is empty"
+        # assert len(batch) > 1, "use get_logit_mat instead"
+        model_kwargs = self.prepare_model_kwargs_batch(batch)
+        # assert model_kwargs["input_ids"].shape[0] == len(batch)
+        # assert model_kwargs["attention_mask"].shape[0] == len(batch)
+        # max_len = max([len(sequence) for sequence in batch])
+        # assert model_kwargs["input_ids"].shape[1] == max_len + self.group_size - 1, \
+        #     f"{model_kwargs['input_ids'].shape[1]} != {max_len + self.group_size - 1}"
         with no_grad():
             all_logits: Tensor = self.model(
                 **model_kwargs
             ).logits
+        # assert all_logits.shape[0] == len(batch), f"The shape of the logits is not correct, " \
+        #                                            f"expected {len(batch)} but got {all_logits.shape[0]}"
+        # assert all_logits.shape[2] == self.vocab_size, f"The shape of the logits is not correct, " \
+        #                                                f"expected {self.vocab_size} but got {all_logits.shape[2]}"
         unscaled_relevant_logits: List[Tensor] = []
-        for i, sequence in enumerate(tokens):
+        for i, sequence in enumerate(batch):
             # chose stop_index such that the target_length of the sequence is group_size
-            curr_relevant_logits = all_logits[i, len(sequence) - 1:len(sequence) + self.group_size, :self.vocab_size]
-            unscaled_relevant_logits.append(curr_relevant_logits.unsqueeze(0))
-        return safe_cat_batch(unscaled_relevant_logits)
+            curr_relevant_logits = all_logits[
+                                   i, len(sequence) - 1:len(sequence) + self.group_size, :self.vocab_size
+                                   ]
+            # assert curr_relevant_logits.shape == (self.group_size, self.vocab_size), \
+            #     f"curr_relevant_logits.shape = {curr_relevant_logits.shape} != " \
+            #     f"(self.group_size, self.vocab_size) = ({self.group_size}, {self.vocab_size})"
+            un_squeezed_logits = curr_relevant_logits.unsqueeze(0)
+            # assert un_squeezed_logits.shape == (1, self.group_size, self.vocab_size), \
+            #     f"un_squeezed_logits.shape = {un_squeezed_logits.shape} != " \
+            #     f"(1, self.group_size, self.vocab_size) = (1, {self.group_size}, {self.vocab_size})"
+            unscaled_relevant_logits.append(un_squeezed_logits)
+
+        answer = cat(unscaled_relevant_logits)
+        # assert answer.shape == (len(batch), self.group_size, self.vocab_size)
+        return answer
 
     def get_prob_mat_batch(self, tokens: List[TokenIDS], generation_start_indexes: Iterable[int]) -> Tensor:
-        """Given a batch of sequences of tokens,
+        """Given a batch of sequences of batch,
         returns the probability matrix of shape (group_size, vocab_size)
         where logits[i] is the logits vector of the i-th next token
-        complexity: O(batch size * (n^2 + group_size^2)) where n is the target_length of the longest tokens sequence.
+        complexity: O(batch size * (n^2 + group_size^2)) where n is the target_length of the longest batch sequence.
         notice that the logits are not divided by the temperature in this function."""
         unscaled_relevant_logits = self.get_logit_mat_batch(tokens)
+        # assert unscaled_relevant_logits.shape[0] == len(tokens)
+        # assert unscaled_relevant_logits.shape[1] == self.group_size
+        # assert unscaled_relevant_logits.shape[2] == self.vocab_size, \
+        #     f"{unscaled_relevant_logits.shape[2]} != {self.vocab_size}"
         if not self.end_of_sentence_stop:
-            unscaled_relevant_logits[:, :, self.end_of_sentence_id] = -float('inf')
+            try:
+                unscaled_relevant_logits[:, :, self.end_of_sentence_id] = -float('inf')
+            except IndexError as e:
+                print("unscaled_relevant_logits", unscaled_relevant_logits)
+                print("self.end_of_sentence_id", self.end_of_sentence_id)
+                raise e
         penalized_logits = self.repetition_penalty_strategy.call_batch(
             unscaled_relevant_logits, tokens, generation_start_indexes
         )
--- tests/test_generation_utils.py
+++ tests/test_generation_utils.py
@@ -22,29 +22,36 @@ def create_wrapped_models() -> List[GroupedGenerationUtils]:
 @pytest.mark.parametrize("wrapped_model", create_wrapped_models())
 def test_get_prob_mat_batch(wrapped_model: GroupedGenerationUtils):
     single_sequence: TokenIDS = [1, 2, 3, 4, 5]
-    batch_of_size_one: List[TokenIDS] = [single_sequence]
+    batch_of_size_two: List[TokenIDS] = [single_sequence, single_sequence]
     un_batched_prob_mat = wrapped_model.get_prob_mat(single_sequence, 0)
     assert un_batched_prob_mat.shape == (wrapped_model.group_size, wrapped_model.vocab_size)
-    prob_mat_batch = wrapped_model.get_prob_mat_batch(batch_of_size_one, [0])
-    assert prob_mat_batch.shape == (len(batch_of_size_one), wrapped_model.group_size, wrapped_model.vocab_size)
-    assert torch.all(torch.isclose(prob_mat_batch[0], un_batched_prob_mat))
+    prob_mat_batch = wrapped_model.get_prob_mat_batch(batch_of_size_two, [0, 0])
+    assert prob_mat_batch.shape == (len(batch_of_size_two), wrapped_model.group_size, wrapped_model.vocab_size)
+    assert torch.all(torch.isclose(prob_mat_batch[0, :, :], un_batched_prob_mat))
     batch_of_size_two: List[TokenIDS] = [single_sequence, single_sequence]
-    prom_mats = wrapped_model.get_prob_mat_batch(batch_of_size_two)
-    assert torch.all(torch.isclose(prom_mats[0], prom_mats[1]))
+    prob_mats = wrapped_model.get_prob_mat_batch(batch_of_size_two, [0, 0])
+    assert torch.all(torch.isclose(prob_mats[0], prob_mats[1]))
 
 
 @pytest.mark.parametrize("wrapped_model", create_wrapped_models())
 def test_prepare_model_kwargs_batch(wrapped_model: GroupedGenerationUtils):
     tokens: TokenIDS = [1, 2, 3, 4, 5]
     model_kwargs_non_batch = wrapped_model.prepare_model_kwargs(tokens)
-    model_kwargs_batch = wrapped_model.prepare_model_kwargs_batch([tokens])
+    model_kwargs_batch = wrapped_model.prepare_model_kwargs_batch([tokens] * 2)
     assert model_kwargs_non_batch.keys() == model_kwargs_batch.keys()
     non_batch_ids = model_kwargs_non_batch["input_ids"]
     batch_ids = model_kwargs_batch["input_ids"]
-    assert (1, len(tokens) + wrapped_model.group_size - 1) == non_batch_ids.shape == batch_ids.shape
+    assert (1, len(tokens) + wrapped_model.group_size - 1) == non_batch_ids.shape
+    assert (2, len(tokens) + wrapped_model.group_size - 1) == batch_ids.shape
     assert torch.all(torch.eq(non_batch_ids, batch_ids))
     for key in model_kwargs_non_batch.keys():
         assert torch.all(torch.eq(model_kwargs_batch[key], model_kwargs_non_batch[key])), f"key {key} is not equal"
-
     actual_batch = [[1, 2, 3, 4, 5], [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]]
-    wrapped_model.prepare_model_kwargs_batch(actual_batch)
+    max_len = max([len(seq) for seq in actual_batch])
+    model_kwargs = wrapped_model.prepare_model_kwargs_batch(actual_batch)
+    input_ids = model_kwargs["input_ids"]
+    assert (len(actual_batch), max_len + wrapped_model.group_size - 1) == input_ids.shape
+
+
+if __name__ == '__main__':
+    pytest.main()
