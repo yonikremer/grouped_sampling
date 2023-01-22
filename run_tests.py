501d3bde91189134248c263368e17b4cdcfe4964
yoni kremer <yoni.kremer@gmail.com>
2023-01-22 18:05:35 +0200

    improved testing experience

--- run_tests.py
+++ run_tests.py
@@ -3,27 +3,41 @@ import sys
 
 import pytest
 from _pytest.config import ExitCode
-import coverage
+from coverage import Coverage
 
 script_dir = os.path.dirname(os.path.realpath(__file__))
 
 
 def main() -> bool:
     """Run all tests and return True if all tests passed, False otherwise."""
-    cov = coverage.Coverage()
+    cov = Coverage(exclude=["*/tests/*", "*/__init__.py", os.path.join(script_dir, "web_app", "*"), "*raise*"])
     cov.start()
 
-    exitcode: ExitCode = pytest.main([script_dir, "--verbose", "--color=yes"])
+    exitcode: ExitCode = pytest.main(
+        [
+            script_dir,
+            "--verbose",
+            "--color=yes",
+            "--tb=short"
+        ]
+    )
+
     cov.stop()
     cov.save()
-    report = cov.report()
+    report = cov.report(
+        show_missing=True,
+        skip_covered=True,
+        omit=["*/tests/*", "*/__init__.py", os.path.join(script_dir, "web_app", "*")],
+        skip_empty=True,
+        include=[os.path.join(script_dir, "src", "*")],
+    )
+    cov.html_report(directory=os.path.join(script_dir, "coverage_html_report"))
     return exitcode == ExitCode.OK and report in (1, 100)
 
 
 if __name__ == '__main__':
-    webapp_folder = os.path.join(script_dir, "web_app")
-    sys.path.append(webapp_folder)
-    if main():  # if all tests passed
+    all_tests_passed = main()
+    if all_tests_passed:  # if all tests passed
         sys.exit(0)
     else:
         sys.exit(1)  # if any test failed / error occurred / etc.
