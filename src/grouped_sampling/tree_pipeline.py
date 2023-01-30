d3ff13bf8ed6aae44d07ddadbe517bb1071947dd
yoni kremer <yoni.kremer@gmail.com>
2023-01-30 16:50:43 +0200

    added a missing import

--- src/grouped_sampling/tree_pipeline.py
+++ src/grouped_sampling/tree_pipeline.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 import sys
 from math import ceil
-from typing import Any, Dict, List, Optional, Sequence, Tuple
+from typing import Any, Dict, List, Optional, Sequence, Tuple, Iterable
 
 from torch import LongTensor, Tensor
 
