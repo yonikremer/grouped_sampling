e6bf83e2ee1523fc2c71721a449972fbb946f85c
yoni kremer <yoni.kremer@gmail.com>
2023-01-30 22:42:11 +0200

    renamed generator to pipeline

--- tests/test_pipelines.py
+++ tests/test_pipelines.py
@@ -25,16 +25,16 @@ long_prompt: str = "This is a very long text. " * 512
 EDGE_CASE_PROMPTS = [long_prompt, repeating_prompt]
 TEST_PROMPT = "This is a test prompt."
 TEST_PIPELINE = GroupedSamplingPipeLine(
-        model_name=MODEL_NAME,
-        group_size=GROUP_SIZES[0],
-        top_k=TOP_KS[0],
-        top_p=None,
-        temp=TEMPERATURES[0],
-        answer_length_multiplier=1.0,
-    )
+    model_name=MODEL_NAME,
+    group_size=GROUP_SIZES[0],
+    top_k=TOP_KS[0],
+    top_p=None,
+    temp=TEMPERATURES[0],
+    answer_length_multiplier=1.0,
+)
 
 
-def create_text_generators(
+def create_pipelines(
 ) -> Generator[GroupedGenerationPipeLine, None, None]:
     top_k_sampling_gen = GroupedSamplingPipeLine(
         model_name=MODEL_NAME,
@@ -78,30 +78,30 @@ def create_text_generators(
 
 
 def test_initialization():
-    for _ in create_text_generators():
+    for _ in create_pipelines():
         continue
 
 
 def test_str():
-    for curr_text_generator in create_text_generators():
-        assert isinstance(str(curr_text_generator),
-                          str), f"{str(curr_text_generator)} is not a string"
+    for curr_pipeline in create_pipelines():
+        assert isinstance(str(curr_pipeline),
+                          str), f"{str(curr_pipeline)} is not a string"
 
 
 @pytest.mark.parametrize(
-    "curr_text_generator, edge_case_prompt",
-    product(islice(create_text_generators(), 3, 5), EDGE_CASE_PROMPTS),
+    "curr_pipeline, edge_case_prompt",
+    product(islice(create_pipelines(), 3, 5), EDGE_CASE_PROMPTS),
 )
-def test_edge_cases(curr_text_generator: GroupedGenerationPipeLine,
+def test_edge_cases(curr_pipeline: GroupedGenerationPipeLine,
                     edge_case_prompt: str):
-    answer = curr_text_generator(
+    answer = curr_pipeline(
         prompt_s=edge_case_prompt,
-        max_new_tokens=curr_text_generator.wrapped_model.group_size * 2,
+        max_new_tokens=curr_pipeline.wrapped_model.group_size * 2,
         return_full_text=True,
     )["generated_text"]
     assert isinstance(
         answer, str
-    ), f"{answer} is not a string. curr_generator: {str(curr_text_generator)}"
+    ), f"{answer} is not a string. curr_pipeline: {str(curr_pipeline)}"
     assert len(answer) > len(
         edge_case_prompt
     ), f"{answer} is not a not longer than {edge_case_prompt}"
@@ -112,9 +112,9 @@ def test_edge_cases(curr_text_generator: GroupedGenerationPipeLine,
 
 def test_pre_and_post_process():
     """Tests the different returning options"""
-    generator: GroupedGenerationPipeLine = TEST_PIPELINE
+    pipeline: GroupedGenerationPipeLine = TEST_PIPELINE
     prompt: str = "This is a test prompt"
-    returned_val: CompletionDict = generator(
+    returned_val: CompletionDict = pipeline(
         prompt_s=prompt,
         max_new_tokens=10,
         return_tensors=True,
@@ -136,14 +136,14 @@ def test_pre_and_post_process():
     assert returned_text.startswith(
         prompt), f"{returned_text} doesn't start with {prompt}"
     prompt_tokens: Tensor
-    prompt_tokens, _, _, _ = generator.pre_processing_strategy(prompt)
+    prompt_tokens, _, _, _ = pipeline.pre_processing_strategy(prompt)
     assert equal(
         generated_token_ids[:len(prompt_tokens)], prompt_tokens
     ), f"{returned_val['generated_token_ids'].tolist()} is not equal to {prompt_tokens}"
     assert isinstance(
         generated_token_ids[:len(prompt_tokens)],
         Tensor), f"{returned_val['generated_token_ids']} is not a tensor"
-    post_processed_text: str = generator.post_processing_strategy(
+    post_processed_text: str = pipeline.post_processing_strategy(
         token_ids=generated_token_ids.tolist(),
         num_new_tokens=10,
         prompt_len=len(prompt_tokens),
@@ -155,7 +155,7 @@ def test_pre_and_post_process():
     assert (returned_text == post_processed_text
             ), f"{returned_text} is not equal to {post_processed_text}"
 
-    returned_val: CompletionDict = generator(
+    returned_val: CompletionDict = pipeline(
         prompt_s=prompt,
         max_new_tokens=10,
         return_tensors=True,
@@ -165,7 +165,7 @@ def test_pre_and_post_process():
     returned_text = returned_val["generated_text"]
     assert not returned_text.startswith(
         prompt), f"{returned_text} doesn't start with {prompt}"
-    prompt_tokens, _, _, _ = generator.pre_processing_strategy(prompt)
+    prompt_tokens, _, _, _ = pipeline.pre_processing_strategy(prompt)
     assert not equal(
         returned_val["generated_token_ids"][:len(prompt_tokens)], prompt_tokens
     ), f"{returned_val['generated_token_ids'][:len(prompt_tokens)]} is equal to {prompt_tokens}"
@@ -173,10 +173,10 @@ def test_pre_and_post_process():
 
 def test_prefix():
     """Tests that the prefix option of the methods __call__ and preprocess works"""
-    generator: GroupedGenerationPipeLine = TEST_PIPELINE
+    pipeline: GroupedGenerationPipeLine = TEST_PIPELINE
     prompt: str = "test prompt"
     prefix = "This is a "
-    answer: CompletionDict = generator(
+    answer: CompletionDict = pipeline(
         prompt_s=prompt,
         max_new_tokens=10,
         return_tensors=True,
@@ -188,7 +188,7 @@ def test_prefix():
             ), f"{answer['generated_text']} doesn't contain {prompt}"
     assert (prefix not in answer["generated_text"]
             ), f"{answer['generated_text']} doesn't contain {prefix}"
-    answer: str = generator(
+    answer: str = pipeline(
         prompt_s=prompt,
         max_new_tokens=10,
         return_tensors=False,
@@ -202,10 +202,10 @@ def test_prefix():
 
 def test_postfix():
     """Tests that the postfix option of the methods __call__ and preprocess works"""
-    generator: GroupedGenerationPipeLine = TEST_PIPELINE
+    pipeline: GroupedGenerationPipeLine = TEST_PIPELINE
     prompt: str = "This is a"
     postfix = " Test postfix"
-    answer: str = generator(
+    answer: str = pipeline(
         prompt_s=prompt,
         max_new_tokens=10,
         return_tensors=False,
@@ -215,7 +215,7 @@ def test_postfix():
     )["generated_text"]
     assert prompt in answer, f"{answer} doesn't contain {prompt}"
     assert postfix not in answer, f"{answer} contain {postfix}"
-    answer = generator(
+    answer = pipeline(
         prompt_s=prompt,
         max_new_tokens=10,
         return_tensors=False,
@@ -228,9 +228,9 @@ def test_postfix():
 
 
 def test_max_new_tokens_is_none():
-    curr_text_generator = TEST_PIPELINE
-    curr_text_generator.wrapped_model.end_of_sentence_stop = True
-    answer: CompletionDict = curr_text_generator(
+    curr_pipeline = TEST_PIPELINE
+    curr_pipeline.wrapped_model.end_of_sentence_stop = True
+    answer: CompletionDict = curr_pipeline(
         prompt_s=TEST_PROMPT,
         max_new_tokens=None,
         return_tensors=False,
@@ -247,14 +247,14 @@ def test_max_new_tokens_is_none():
 
 def test_call_many_prompts():
     """Tests the __call__ method when many prompts are given"""
-    generator = TEST_PIPELINE
+    pipeline = TEST_PIPELINE
     PROMPTS: List[str] = [
         "This is a test prompt", "This is another test prompt"
     ]
-    answers: List[CompletionDict] = generator(prompt_s=PROMPTS,
-                                              max_new_tokens=10,
-                                              return_text=True,
-                                              return_full_text=True)
+    answers: List[CompletionDict] = pipeline(prompt_s=PROMPTS,
+                                             max_new_tokens=10,
+                                             return_text=True,
+                                             return_full_text=True)
     assert isinstance(answers, list), f"{answers} is not a list"
     for prompt, answer in zip(PROMPTS, answers):
         assert isinstance(answer, dict), f"{answer} is not a dict"
@@ -265,9 +265,9 @@ def test_call_many_prompts():
         assert answer["generated_text"].startswith(
             prompt), f"{answer['generated_text']} doesn't start with {prompt}"
 
-    generator.max_batch_size = 2
+    pipeline.max_batch_size = 2
     prompts = PROMPTS * 2 + [PROMPTS[0]]
-    answers: List[CompletionDict] = generator(
+    answers: List[CompletionDict] = pipeline(
         prompt_s=prompts,
         max_new_tokens=10,
         return_tensors=True,
@@ -288,12 +288,12 @@ def test_call_many_prompts():
 
 
 def test_call_batch():
-    generator = TEST_PIPELINE
+    pipeline = TEST_PIPELINE
     empty_batch = []
-    empty_batch_answer = generator(empty_batch)
+    empty_batch_answer = pipeline(empty_batch)
     assert empty_batch_answer == [], f"{empty_batch_answer} is not an empty list"
     batch_of_size_one = [TEST_PROMPT]
-    batch_of_size_one_answer = generator(batch_of_size_one, return_full_text=True)
+    batch_of_size_one_answer = pipeline(batch_of_size_one, return_full_text=True)
     assert isinstance(batch_of_size_one_answer, list), f"{batch_of_size_one_answer} is not a list"
     assert len(batch_of_size_one_answer) == 1, f"{batch_of_size_one_answer} doesn't have a length of 1"
     assert isinstance(batch_of_size_one_answer[0], dict), f"{batch_of_size_one_answer[0]} is not a dict"
@@ -306,7 +306,7 @@ def test_call_batch():
     assert len(batch_of_size_one_answer[0]["generated_text"]) > len(TEST_PROMPT), \
         f"{batch_of_size_one_answer[0]['generated_text']} is too short"
     batch_of_size_two = [TEST_PROMPT, TEST_PROMPT]
-    batch_of_size_two_answer = generator(batch_of_size_two, return_full_text=True)
+    batch_of_size_two_answer = pipeline(batch_of_size_two, return_full_text=True)
     assert isinstance(batch_of_size_two_answer, list), f"{batch_of_size_two_answer} is not a list"
     assert len(batch_of_size_two_answer) == 2, f"{batch_of_size_two_answer} doesn't have a length of 2"
     for answer in batch_of_size_two_answer:
@@ -322,43 +322,43 @@ def test_call_batch():
             f"{answer['generated_text']} is too short"
 
 
-@pytest.mark.parametrize("curr_text_generator", create_text_generators())
-def test_calling_generators(curr_text_generator):
-    answer = curr_text_generator(
+@pytest.mark.parametrize("curr_pipeline", create_pipelines())
+def test_calling_pipelines(curr_pipeline):
+    answer = curr_pipeline(
         prompt_s=PROMPT,
-        max_new_tokens=curr_text_generator.wrapped_model.group_size * 2,
+        max_new_tokens=curr_pipeline.wrapped_model.group_size * 2,
     )["generated_text"]
     assert isinstance(
         answer, str
-    ), f"{answer} is not a string. curr_text_generator: {str(curr_text_generator)}"
+    ), f"{answer} is not a string. curr_pipeline: {str(curr_pipeline)}"
     assert answer.startswith(PROMPT), f"{answer} doesn't start with {PROMPT}"
     assert len(answer) > len(PROMPT), f"{answer} is too short"
 
 
-@pytest.mark.parametrize("curr_text_generator", create_text_generators())
-def test_dict_conversions(curr_text_generator: GroupedGenerationPipeLine):
-    new_generator = type(curr_text_generator).from_dict(
-        curr_text_generator.as_dict())
-    assert type(curr_text_generator) is type(new_generator)
-    assert curr_text_generator.model_name == new_generator.model_name
-    assert (curr_text_generator.wrapped_model.group_size ==
-            new_generator.wrapped_model.group_size)
-    assert curr_text_generator.wrapped_model.temp == new_generator.wrapped_model.temp
-    assert curr_text_generator.generation_type == new_generator.generation_type
-    assert (curr_text_generator.wrapped_model.end_of_sentence_id ==
-            new_generator.wrapped_model.end_of_sentence_id)
-    assert (curr_text_generator.wrapped_model.end_of_sentence_stop ==
-            new_generator.wrapped_model.end_of_sentence_stop)
-    assert (curr_text_generator.wrapped_model.max_input_len ==
-            new_generator.wrapped_model.max_input_len)
-    assert (curr_text_generator.answer_length_multiplier ==
-            new_generator.answer_length_multiplier)
-    assert (curr_text_generator.wrapped_model.vocab_size ==
-            new_generator.wrapped_model.vocab_size)
-    if hasattr(curr_text_generator, "top_k"):
-        assert curr_text_generator.top_k == new_generator.top_k
-    if hasattr(curr_text_generator, "top_p"):
-        assert curr_text_generator.top_p == new_generator.top_p
+@pytest.mark.parametrize("curr_pipeline", create_pipelines())
+def test_dict_conversions(curr_pipeline: GroupedGenerationPipeLine):
+    new_pipeline = type(curr_pipeline).from_dict(
+        curr_pipeline.as_dict())
+    assert type(curr_pipeline) is type(new_pipeline)
+    assert curr_pipeline.model_name == new_pipeline.model_name
+    assert (curr_pipeline.wrapped_model.group_size ==
+            new_pipeline.wrapped_model.group_size)
+    assert curr_pipeline.wrapped_model.temp == new_pipeline.wrapped_model.temp
+    assert curr_pipeline.generation_type == new_pipeline.generation_type
+    assert (curr_pipeline.wrapped_model.end_of_sentence_id ==
+            new_pipeline.wrapped_model.end_of_sentence_id)
+    assert (curr_pipeline.wrapped_model.end_of_sentence_stop ==
+            new_pipeline.wrapped_model.end_of_sentence_stop)
+    assert (curr_pipeline.wrapped_model.max_input_len ==
+            new_pipeline.wrapped_model.max_input_len)
+    assert (curr_pipeline.answer_length_multiplier ==
+            new_pipeline.answer_length_multiplier)
+    assert (curr_pipeline.wrapped_model.vocab_size ==
+            new_pipeline.wrapped_model.vocab_size)
+    if hasattr(curr_pipeline, "top_k"):
+        assert curr_pipeline.top_k == new_pipeline.top_k
+    if hasattr(curr_pipeline, "top_p"):
+        assert curr_pipeline.top_p == new_pipeline.top_p
 
 
 if __name__ == "__main__":
