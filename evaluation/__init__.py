7af64d6742b8b5cdfc00725c372177053af38558
Yoni Kremer <76044840+yonikremer@users.noreply.github.com>
2023-02-02 22:35:08 +0200

    Fixed the docstring style

--- evaluation/__init__.py
+++ evaluation/__init__.py
@@ -34,8 +34,10 @@ DATASET_NAME = "ted_talks_iwslt"
 
 
 def lang_code_to_name(language_code: str) -> str:
-    """Converts language's ISO_639_1 code to language name
-    Raises KeyError if language code is not one of the languages in the ted_talks_iwslt dataset"""
+    """
+    Converts language's ISO_639_1 code to language name
+    Raises KeyError if language code is not one of the languages in the ted_talks_iwslt dataset
+    """
     ISO_639_1 = {
         "de": "German",
         "en": "English",
