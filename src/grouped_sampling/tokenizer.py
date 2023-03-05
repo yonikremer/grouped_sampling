f7475e7f136ab874c5d243d8dbaa13c93e8d6d32
yoni <yoni.kremer@gmail.com>
2023-03-05 17:13:31 +0200

    A lot of bug fixed

--- src/grouped_sampling/tokenizer.py
+++ src/grouped_sampling/tokenizer.py
@@ -1,3 +1,10 @@
+import json
+import os
+from functools import lru_cache
+from typing import Optional, Dict
+
+import requests
+from huggingface_hub.utils import validate_repo_id, HFValidationError
 from transformers import PreTrainedTokenizer, AutoTokenizer, AutoConfig
 
 
@@ -45,24 +52,100 @@ def get_end_of_text_id(tokenizer: PreTrainedTokenizer, config: AutoConfig):
     )
 
 
+def get_model_config(model_name: str) -> Optional[dict]:
+    """Returns the config.json file of a model from huggingface as a dictionary"""
+    base_url = f"https://huggingface.co/{model_name}/raw/main/config.json"
+    try:
+        config_json_string: str = requests.get(base_url).text
+    except (requests.exceptions.ConnectionError, requests.exceptions.ReadTimeout):
+        return None
+    return json.loads(config_json_string)
+
+
+def get_tokenizer_config(model_name: str) -> Optional[dict]:
+    """
+    Returns the tokenizer_config.json file of a tokenizer from huggingface as a dictionary
+    """
+    base_url = f"https://huggingface.co/{model_name}/raw/main/tokenizer_config.json"
+    try:
+        config_json_string: str = requests.get(base_url).text
+    except (requests.exceptions.ConnectionError, requests.exceptions.ReadTimeout):
+        return None
+    return json.loads(config_json_string)
+
+
+def is_valid_model_name(model_name: str) -> bool:
+    """Returns True if the model name is valid"""
+    if "output" in model_name:
+        return False
+    if model_name in {
+        ".",
+        "model_medium_300_hf",
+        "DialoGPT-small-house",
+        "emo-bot",
+        "sberbank-ai/rugpt3_medium_gpt2_based",
+        "Grossmend/rudialogpt3_medium_based_on_gpt2",
+        "workflow"
+    }:
+        return False
+    try:
+        validate_repo_id(model_name)
+    except HFValidationError:
+        return False
+    if model_name.count("/") > 1:
+        return False
+    return True
+
+
+def get_model_name_from_repo(repo_id: str) -> str:
+    try:
+        model_config = get_model_config(repo_id)
+    except Exception:
+        return repo_id
+    repo_id_no_org = repo_id.split("/")[-1]
+    if model_config is not None and "_name_or_path" in model_config.keys():
+        config_model_name = model_config["_name_or_path"]
+        if is_valid_model_name(config_model_name) and config_model_name != repo_id_no_org:
+            return config_model_name
+    return repo_id
+
+
+def get_tokenizer_name_from_repo(repo_id: str) -> str:
+    try:
+        tokenizer_config = get_tokenizer_config(repo_id)
+    except Exception:
+        return repo_id
+    repo_id_no_org = repo_id.split("/")[-1]
+    if tokenizer_config is not None and "name_or_path" in tokenizer_config.keys():
+        config_model_name = tokenizer_config["name_or_path"]
+        if is_valid_model_name(config_model_name) and config_model_name != repo_id_no_org:
+            return config_model_name
+    return repo_id
+
+
+@lru_cache(maxsize=1)
+def get_special_cases() -> Dict[str, str]:
+    special_cases_file = os.path.join(os.path.dirname(__file__), "model_to_tokenizer.json")
+    with open(special_cases_file) as f:
+        return json.loads(f.read())
+
+
+# noinspection PyProtectedMember
 def get_tokenizer_name(
         model_name: str,
 ) -> str:
     """Returns a tokenizer name based on the model name"""
-    config = AutoConfig.from_pretrained(model_name, trust_remote_code=True)
-    if hasattr(config, "tokenizer_class") and config.tokenizer_class is not None:
-        print(f"Using tokenizer class {config.tokenizer_class} for model {model_name}")
-        return config.tokenizer_class
-    if model_name == "NovelAI/genji-jp":
-        return "EleutherAI/gpt-j-6B"
-    if model_name in {"NovelAI/genji-python-6B", "luke-thorburn/suggest-conclusion-full-finetune"}:
-        return "EleutherAI/gpt-neo-2.7B"
-    if model_name in {"mustapha/distilgpt2-finetuned-wikitext2", "Aleksandar1932/distilgpt2-rock", "AntonClaesson/movie-plot-generator"}:
-        return "distilgpt2"
-    if model_name in {"mymusise/CPM-Generate-distill"}:
-        return "TsinghuaAI/CPM-Generate"
+    special_cases = get_special_cases()
+    if model_name in special_cases.keys():
+        return special_cases[model_name]
     if model_name.startswith("Aleksandar1932/gpt2") or model_name.startswith("Azaghast/GPT2"):
         return "gpt2"
+    tokenizer_name_from_repo = get_tokenizer_name_from_repo(model_name)
+    if tokenizer_name_from_repo != model_name:
+        return get_tokenizer_name(tokenizer_name_from_repo)
+    model_name_from_repo = get_model_name_from_repo(model_name)
+    if model_name_from_repo != model_name:
+        return get_tokenizer_name(model_name_from_repo)
     return model_name
 
 
