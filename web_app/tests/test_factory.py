c4e52c14d8910ea6ef00ef8d3e15931f56541dfb
yoni <yoni.kremer@gmail.com>
2023-04-18 14:35:31 +0300

    deleted the web app from the project

--- web_app/tests/test_factory.py
+++
@@ -1,8 +0,0 @@
-"""configures the tests"""
-from web_app.flaskr.__init__ import create_app
-
-
-def test_config() -> None:
-    """Test create_app without passing test config."""
-    assert not create_app().testing
-    assert create_app({'TESTING': True}).testing
