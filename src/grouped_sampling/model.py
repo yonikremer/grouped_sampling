327d85330ca7130996f88977d08691dc31a52f21
yonikremer <yoni.kremer@gmail.com>
2023-07-02 15:44:04 +0300

    compiling the model and using evaluation mode

--- src/grouped_sampling/model.py
+++ src/grouped_sampling/model.py
@@ -1,8 +1,9 @@
 from warnings import warn
 
-from torch import cuda
+from torch import cuda, compile
+from torch._dynamo import OptimizedModule
 from torch.cuda import OutOfMemoryError
-from transformers import PreTrainedModel, AutoModelForCausalLM
+from transformers import AutoModelForCausalLM
 
 
 def get_model(
@@ -10,22 +11,16 @@ def get_model(
         load_in_8bit: bool = False,
         use_cuda: bool = True,
         **kwargs,
-) -> PreTrainedModel:
+) -> OptimizedModule:
     using_8bit = use_cuda and load_in_8bit and cuda.is_available()
+    full_model_kwargs = {
+        "pretrained_model_name_or_path": model_name,
+        "load_in_8bit": using_8bit,
+        "resume_download": True,
+        **kwargs,
+    }
     if using_8bit:
-        full_model_kwargs = {
-            "pretrained_model_name_or_path": model_name,
-            "load_in_8bit": using_8bit,
-            "device_map": "auto",
-            "resume_download": True,
-            ** kwargs,
-        }
-    else:
-        full_model_kwargs = {
-            "pretrained_model_name_or_path": model_name,
-            "load_in_8bit": using_8bit,
-            ** kwargs,
-        }
+        full_model_kwargs["device_map"] = "auto"
     model = AutoModelForCausalLM.from_pretrained(**full_model_kwargs)
     if use_cuda and cuda.is_available():
         try:
@@ -34,4 +29,6 @@ def get_model(
             warn("The model is too large for your GPU, using the CPU instead.")
     else:
         warn("CUDA is not avilable, using the CPU instead")
+    model = model.eval()
+    model = compile(model)
     return model
