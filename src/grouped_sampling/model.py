e14a152e26da4b98fd5504b86bd07b35728b4246
yonikremer <yoni.kremer@gmail.com>
2023-07-03 17:33:41 +0300

    added docstring

--- src/grouped_sampling/model.py
+++ src/grouped_sampling/model.py
@@ -1,7 +1,6 @@
 from warnings import warn
 
 from torch import cuda, compile
-from torch._dynamo import OptimizedModule
 from torch.cuda import OutOfMemoryError
 from transformers import AutoModelForCausalLM
 
@@ -10,7 +9,16 @@ def get_model(
         model_name: str,
         load_in_8bit: bool = False,
         **kwargs,
-) -> OptimizedModule:
+):
+    """
+    Load a model from the huggingface model hub, and compile it for faster inference.
+    args:
+        model_name: the name of the model to load.
+        load_in_8bit: if True, the model will be loaded in 8bit mode, which is faster but less accurate.
+        **kwargs: additional arguments to pass to the model's from_pretrained method.
+    returns:
+        the loaded model.
+    """
     using_8bit = load_in_8bit and cuda.is_available()
     full_model_kwargs = {
         "pretrained_model_name_or_path": model_name,
