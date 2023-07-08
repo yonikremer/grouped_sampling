b7d9e2e1c2a0378639d924f9b95e69024eb80bbe
yonikremer <yoni.kremer@gmail.com>
2023-07-08 15:31:05 +0300

    added setup_method to TestGetModel

--- tests/test_get_model.py
+++ tests/test_get_model.py
@@ -48,6 +48,19 @@ Additional aspects:
 
 class TestGetModel:
     @staticmethod
+    def setup_method():
+        os.environ["TOKENIZERS_PARALLELISM"] = "false"
+        new_path = "/home/yoni/miniconda3/envs/grouped_sampling_new/lib/python3.10/site-packages/nvidia/cuda_runtime/lib"
+        if "LD_LIBRARY_PATH" in os.environ:
+            os.environ["LD_LIBRARY_PATH"] += ":" + new_path
+        else:
+            os.environ["LD_LIBRARY_PATH"] = new_path
+        import bitsandbytes
+        assert (
+            bitsandbytes.COMPILED_WITH_CUDA
+        ), "bitsandbytes was not compiled with CUDA"
+
+    @staticmethod
     def validate_model(model):
         assert isinstance(
             model, OptimizedModule
