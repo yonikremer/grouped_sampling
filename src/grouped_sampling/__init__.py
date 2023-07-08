4c79b53a449fc5130b4b5558e68ae1ceb208511c
yonikremer <yoni.kremer@gmail.com>
2023-07-08 13:26:36 +0300

    deleted support check

--- src/grouped_sampling/__init__.py
+++
@@ -1,9 +0,0 @@
-from .support_check import (
-    DEFAULT_MIN_NUMBER_OF_DOWNLOADS,
-    DEFAULT_MIN_NUMBER_OF_LIKES,
-    UnsupportedModelNameException,
-    check_support,
-    get_full_models_list,
-    is_supported,
-    unsupported_model_name_error_message,
-)
