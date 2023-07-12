0966a03d7589195b1aa742ec1910013308d61038
yonikremer <yoni.kremer@gmail.com>
2023-07-12 11:27:11 +0300

    evaluate_translation.py using BatchPipeLine

--- evaluation/evaluate_translation.py
+++ evaluation/evaluate_translation.py
@@ -10,7 +10,7 @@ from datasets import Dataset, get_dataset_config_names
 from evaluation.experiment_manager import ExperimentManager
 from evaluation import lang_code_to_name, process_translation_data, DATASET_NAME, create_pipeline, \
     disable_progress_bars, get_experiment_parameters
-from src.grouped_sampling import BatchEndToEndSingleSequencePipeLine
+from src.grouped_sampling import BatchPipeLine
 
 disable_progress_bars()
 
@@ -19,7 +19,7 @@ def sub_experiment_half(
         my_evaluator: TranslationEvaluator,
         sub_set_half: Dataset,
         in_lang_code: str, out_lang_code: str,
-        generator: BatchEndToEndSingleSequencePipeLine,
+        generator: BatchPipeLine,
         manager: ExperimentManager) -> None:
     input_lang_name, output_lang_name = lang_code_to_name(in_lang_code), lang_code_to_name(out_lang_code)
     prefix = f"Translate {input_lang_name} to {output_lang_name}: \n {input_lang_name}: "
@@ -37,7 +37,7 @@ def sub_experiment_half(
 
 
 def run_experiment(
-        pipeline: BatchEndToEndSingleSequencePipeLine,
+        pipeline: BatchPipeLine,
         my_evaluator: TranslationEvaluator,
         sub_sut_names: List[str],
         debug: bool,
@@ -76,7 +76,7 @@ def main(debug: bool = __debug__) -> None:
     sub_sut_names = get_dataset_config_names(DATASET_NAME)
     if debug:
         sub_sut_names = sub_sut_names[:1]
-    curr_text_generator = create_pipeline()
+    curr_text_generator = create_pipeline(max_batch_size=32)
     curr_evaluator = create_evaluator()
     parameters = get_experiment_parameters()
     run_experiment(curr_text_generator, curr_evaluator, sub_sut_names, debug, parameters)
