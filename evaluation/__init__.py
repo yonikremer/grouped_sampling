0fb872f17226192b028701d65145372fe87619e3
yonikremer <yoni.kremer@gmail.com>
2023-07-16 13:31:21 +0300

    moved get_comet_api_key to a separate file

--- evaluation/__init__.py
+++ evaluation/__init__.py
@@ -43,28 +43,6 @@ def lang_code_to_name(language_code: str) -> str:
     return iso_639_1[language_code]
 
 
-def get_comet_api_key() -> str:
-    """
-    Returns the Comet API key from the file "final_project/evaluate/comet_api_key.txt"
-    if this file does not exist, asks the user to enter the key manually and saves it to the file
-    """
-    if using_kaggle:
-        return UserSecretsClient().get_secret("comet_ml_api_key")
-    if os.getcwd() == "/content":
-        # if running on colab
-        api_key_file = "final_project/evaluation/comet_ml_api_key.txt"
-    else:
-        # if running locally
-        api_key_file = "comet_ml_api_key.txt"
-    if os.path.exists(api_key_file):
-        with open(api_key_file, "r") as f:
-            return f.read().strip()
-    api_key = input("Please enter your api_key for comet ml: ")
-    with open(api_key_file, "w") as f:
-        f.write(api_key)
-    return api_key
-
-
 def get_project_name(debug: bool = __debug__) -> str:
     if debug:
         warn("RUNNING IN DEBUG MODE")
