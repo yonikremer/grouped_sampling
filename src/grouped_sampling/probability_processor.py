57e20917db8f3a6e2f0066d0d6eb03bf54fb5b2a
yonikremer <yoni.kremer@gmail.com>
2023-07-17 16:52:24 +0300

    added probability_processor.py

---
+++ src/grouped_sampling/probability_processor.py
@@ -0,0 +1,37 @@
+from abc import ABC
+
+import torch
+from torch import Tensor
+
+
+class ProbabilityProcessor(ABC):
+    def __call__(self, probs: Tensor) -> Tensor:
+        """
+        Args:
+            probs: Tensor of shape (batch_size, output_length, vocab_size).
+        Returns:
+            A Tensor of shape (batch_size, output_length, vocab_size) with the proccessed probs.
+        """
+        raise NotImplementedError
+
+    def _validate_probs(self, probs):
+        if not torch.is_tensor(probs):
+            raise TypeError("probs must be a tensor")
+        if probs.dim() != 3:
+            raise ValueError(
+                f"probs must be a 3 dimensional tensor. Got {probs.dim()} dimensions."
+            )
+        if min(probs.shape) == 0:
+            raise ValueError(
+                f"probs must be a non empty tensor. Got {probs.shape} shape."
+            )
+        if (torch.isnan(probs)).any():
+            raise ValueError("probs must not contain NaN values.")
+        if (torch.isinf(probs)).any():
+            raise ValueError("probs must not contain Inf values.")
+        if (probs < 0).any():
+            raise ValueError("probs must not contain negative values.")
+        if (probs > 1).any():
+            raise ValueError("probs must not contain values greater than 1.")
+        if (probs.sum(dim=-1) > 1).any():
+            raise ValueError("probs must sum to 1 or less in the last dimension.")
