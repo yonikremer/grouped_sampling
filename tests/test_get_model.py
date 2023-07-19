78ff702e31e6f4687222984203ecb987fb9ff766
yonikremer <yoni.kremer@gmail.com>
2023-07-19 14:35:41 +0300

    deleted test_huge_model

--- tests/test_get_model.py
+++ tests/test_get_model.py
@@ -149,9 +149,3 @@ class TestGetModel:
     def test_opt_dont_use_cache(self):
         model = get_model("facebook/opt-125m")
         assert not model.config.use_cache, "The model config use_cache is not False"
-
-    def test_huge_model(self):
-        warn("This test may take a while")
-        warn("I am using a 15B model because I only have 4GB of VRAM, if you have more, use a bigger model for this test")
-        with pytest.raises(OutOfMemoryError):
-            get_model("WizardLM/WizardCoder-15B-V1.0")
